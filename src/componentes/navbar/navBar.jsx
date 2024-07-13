3cdee057b93499532c610bdc3eb7520c056b84df
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-07-13 17:05:49 -0600

    se agregaron los navlinks para el router

--- src/componentes/navbar/navBar.jsx
+++ src/componentes/navbar/navBar.jsx
@@ -1,9 +1,12 @@
+import { NavLink } from "react-router-dom";
 
 
 export function NavBar(){
     return(
         <div>
-            <h1>Barra de navegacion</h1>
+            <NavLink to={"/"}>Void return</NavLink>
+            <NavLink to={"steve"}>Steve</NavLink>
+            <NavLink to={"docu"}>Docu</NavLink>
         </div>
     )
 }
