d4c811cbaf092b9714cf7cdec16f1d0a88f63ca5
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-07-13 16:28:02 -0600

    se agrego el componente para el footer de la pagina

---
+++ src/componentes/footer/footer.jsx
@@ -0,0 +1,14 @@
+
+
+export function Footer(){
+    return(
+        <div>
+            <div>
+                <p>Contacto</p>
+                <p>Youtube</p>
+                <p>Discord</p>
+                <p>GitHub</p>
+            </div>
+        </div>
+    )
+}
