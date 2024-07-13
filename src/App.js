ea46e553fac2503efe63f4d7092b7907e7ae9dcf
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-07-13 16:11:41 -0600

    se agrego el router del proyecto

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import { DivMain } from './componentes/contenedores/contenedores';
+import { RouterBlog } from './router/router';
+
 
 function App() {
   return (
     <DivMain>
-      
+      <RouterBlog />
     </DivMain>
   );
 }
