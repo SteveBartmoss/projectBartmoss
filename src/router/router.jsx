f6aed0430cd16f43dfe347bf9d7aa715f599816f
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-07-13 17:07:19 -0600

    se agrego una ruta para el perfil de steve

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { NavBar } from "../componentes/navbar/navBar";
 import { Home } from "../pages/home";
 import { Footer } from "../componentes/footer/footer";
+import { Steve } from "../pages/steve";
 
 
 export function RouterBlog(){
@@ -10,6 +11,7 @@ export function RouterBlog(){
             <NavBar />
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="steve" element={<Steve />} />
             </Routes>
             <Footer />
         </BrowserRouter>
