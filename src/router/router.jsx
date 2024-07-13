2c74986e2953edb0d688e9b79ece9dc786a71d4f
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-07-13 16:15:16 -0600

    se agrego la ruta para la pagina de home

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,11 +1,15 @@
-import { BrowserRouter, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { NavBar } from "../componentes/navbar/navBar";
+import { Home } from "../pages/home";
 
 
-export function RoterBlog(){
+export function RouterBlog(){
     return(
         <BrowserRouter>
             <NavBar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+            </Routes>
         </BrowserRouter>
     )
 }
