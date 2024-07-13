45d880d09d32303a9c10035b4d8bf0764ba838a2
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-07-13 17:04:40 -0600

    se agrego una pagina para steve

---
+++ src/pages/steve.jsx
@@ -0,0 +1,10 @@
+
+
+export function Steve(){
+    return(
+        <div>
+            <h1>Steve Bartmoss</h1>
+            <p>No le preguntes a la documentacion preguntame a mi</p>
+        </div>
+    )
+}
