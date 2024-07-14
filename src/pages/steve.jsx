cc2911a0df5fdae22f0b856b33ba298731639a55
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-07-13 21:22:35 -0600

    se retiraron elementos que no eran necesarios

--- src/pages/steve.jsx
+++ src/pages/steve.jsx
@@ -5,8 +5,7 @@ export function Steve(){
     return(
         <div>
             <Portada />
-            <h1>Steve Bartmoss</h1>
-            <p>No le preguntes a la documentacion preguntame a mi</p>
+            
         </div>
     )
 }
