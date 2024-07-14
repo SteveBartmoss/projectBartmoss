277bc53479a4d8d77867dd5bc9075854beca5985
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-07-14 09:40:43 -0600

    se agrego el div principal en el que se mostrar el contenido

--- src/pages/steve.jsx
+++ src/pages/steve.jsx
@@ -1,3 +1,4 @@
+import { DivSection } from "../componentes/contenedores/contenedores";
 import { Portada } from "../componentes/portada/portada";
 
 
@@ -5,7 +6,9 @@ export function Steve(){
     return(
         <div>
             <Portada />
-            
+            <DivSection>
+                <h1>Conetnido del perfil</h1>
+            </DivSection>
         </div>
     )
 }
