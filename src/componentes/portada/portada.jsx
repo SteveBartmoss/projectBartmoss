f01660c30631249f6812f728a8d053d92736e30a
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-07-13 21:16:36 -0600

    se agregaron los estilos y otros componentes para la portada

--- src/componentes/portada/portada.jsx
+++ src/componentes/portada/portada.jsx
@@ -1,14 +1,16 @@
+import './portada.css';
 import { DivCol, DivRow } from "../contenedores/contenedores";
 
 export function Portada({imgPortada}){
     return(
-        <section>
+        <section className="portada">
             <DivRow>
-                <div>
-                    <img />
+                <div className="frame-img">
+                    <img className="img-portada" src="/Assets/steveBartmoss.jpg" /> 
                 </div>
                 <DivCol>
-                    
+                    <h1>Steve Bartmoss</h1>
+                    <p className="text-portada">No le preguntes a la documentacion pregutame a mi</p>
                 </DivCol>
             </DivRow>
         </section>
