cf7a2c2cb51d6df6bdd0e04c38b2f47a98e03879
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-07-13 21:13:53 -0600

    se agrego el componente portada

--- src/pages/steve.jsx
+++ src/pages/steve.jsx
@@ -1,8 +1,10 @@
+import { Portada } from "../componentes/portada/portada";
 
 
 export function Steve(){
     return(
         <div>
+            <Portada />
             <h1>Steve Bartmoss</h1>
             <p>No le preguntes a la documentacion preguntame a mi</p>
         </div>
