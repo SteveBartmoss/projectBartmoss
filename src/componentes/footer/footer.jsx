309aa97ac50517244d652d3fee07d06b07cb3192
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-07-14 09:29:42 -0600

    se agregaron las clases para el conetedor del footer

--- src/componentes/footer/footer.jsx
+++ src/componentes/footer/footer.jsx
@@ -1,14 +1,13 @@
 import './footer.css'
 
-export function Footer(){
-    return(
-        <div>
-            <div>
-                <p>Contacto</p>
-                <p>Youtube</p>
-                <p>Discord</p>
-                <p>GitHub</p>
-            </div>
+export function Footer() {
+    return (
+        <div className="div-footer">
+            <p>Contacto</p>
+            <p>Youtube</p>
+            <p>Discord</p>
+            <p>GitHub</p>
+
         </div>
     )
 }
