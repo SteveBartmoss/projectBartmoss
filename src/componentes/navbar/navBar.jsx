eaee72a23d45e85f97d117a66a303ee225d94ac6
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-07-14 09:16:26 -0600

    se agrego la clase para los estilos

--- src/componentes/navbar/navBar.jsx
+++ src/componentes/navbar/navBar.jsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 
 export function NavBar(){
     return(
-        <div>
+        <div className="div-nav-bar">
             <NavLink to={"/"}>Void return</NavLink>
             <NavLink to={"steve"}>Steve</NavLink>
             <NavLink to={"docu"}>Docu</NavLink>
