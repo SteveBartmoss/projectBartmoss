47dfbc25555d12bf68699a56e4fbc4378c011cf0
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-07-14 09:14:28 -0600

    se agrego la propiedad alt a la imgan de perfil

--- src/componentes/portada/portada.jsx
+++ src/componentes/portada/portada.jsx
@@ -6,7 +6,7 @@ export function Portada({imgPortada}){
         <section className="portada">
             <DivRow>
                 <div className="frame-img">
-                    <img className="img-portada" src="/Assets/steveBartmoss.jpg" /> 
+                    <img alt="imgPerfil" className="img-portada" src="/Assets/steveBartmoss.jpg" /> 
                 </div>
                 <DivCol>
                     <h1 className="title-portada">Steve Bartmoss</h1>
