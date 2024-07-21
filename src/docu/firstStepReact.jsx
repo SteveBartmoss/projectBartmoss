2011743f6f2dc796b6faf0fe8addbb0efde5df40
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-07-21 09:23:57 -0600

    se agrego un articulo de documentacion

---
+++ src/docu/firstStepReact.jsx
@@ -0,0 +1,12 @@
+import { DivSection } from "../componentes/contenedores/contenedores";
+
+
+export function FirstStepReact() {
+    return (
+        <>
+            <DivSection>
+                <h1>Primeros pasos</h1>
+            </DivSection>
+        </>
+    )
+}
