1463ffb7fb5acb3dec60608eaa80b2586667f4d8
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-07-21 16:59:53 -0600

    se actualizo la libreria

--- src/componentes/card/card.jsx
+++ src/componentes/card/card.jsx
@@ -1,3 +1,4 @@
+import { NavLink } from 'react-router-dom';
 import './card.css';
 
 export function Card({children,variant="card-basic"}){
@@ -24,15 +25,25 @@ export function CardText({ children }) {
     );
 }
 
-export function CardLink({ url, text }) {
-    return (
-        <a className="card-link" rel="noopener noreferrer" target="_blank" href={url}>{text}</a>
-    );
+export function CardLink({ url, text, isBlank=false }) {
+    return isBlank ? (
+        <a className="card-link" rel="noopener noreferrer" target="_blank" href={url}>{text}</a> 
+    ) : (
+        <NavLink className="card-link" to={url}>{text}</NavLink>
+    )
 }
 
 export function CardAccions({ children }) {
     return (
         <div className="card-actions">
+            {children}
+        </div>
+    );
+}
+
+export function CardContent({children}){
+    return(
+        <div className="card-content">
             {children}
         </div>
     );
