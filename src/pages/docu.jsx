43b527641fc33e3fa4276447ca5c2f20f8f72a4b
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-07-21 16:42:58 -0600

    se agrego contenido a la seccion

--- src/pages/docu.jsx
+++ src/pages/docu.jsx
@@ -1,21 +1,29 @@
 import { Card, CardAccions, CardLink, CardText, CardTitle } from "../componentes/card/card";
-import { DivSection } from "../componentes/contenedores/contenedores";
+import { Chip } from "../componentes/chips/chip";
+import { DivArticle, DivSection, DivTopicos } from "../componentes/contenedores/contenedores";
+import { Portada } from "../componentes/portada/portada";
 
 export function Docu() {
     return (
         <div>
+            <Portada imgPortada={"/Assets/steveBartmoss.jpg"}
+                title={"La guia del internet"}
+                text={"Por Steve Bartmoss"} />
             <DivSection>
-                <Card variant="card-outlined">
-                    <CardTitle>Primeros Pasos React</CardTitle>
-                    <CardText>
-                        Tus primeros pasos en la libreria de 
-                        react pueden ser algo complejos, pero 
-                        aqui te contamos cosas que te pueden ayudar
-                    </CardText>
-                    <CardAccions>
-                        <CardLink url={'docu/primerospasos'} text={'Ir al articulo'} />
-                    </CardAccions>
-                </Card>
+                <DivTopicos>
+                    <Card variant="card-outlined">
+                        <CardTitle>Primeros Pasos React</CardTitle>
+                        <CardText>
+                            Tus primeros pasos en la libreria de
+                            react pueden ser algo complejos, pero
+                            aqui te contamos cosas que te pueden ayudar
+                        </CardText>
+                        <Chip color="warning">Java Script</Chip>
+                        <CardAccions>
+                            <CardLink url={'docu/primerospasos'} text={'Ir al articulo'} />
+                        </CardAccions>
+                    </Card>
+                </DivTopicos>
             </DivSection>
         </div>
     )
