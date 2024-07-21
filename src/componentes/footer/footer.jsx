7a92a27a5aae2c993e3738bcc79af27fe15dd00a
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-07-20 18:14:29 -0600

    se agregaron anchor tags para el footer

--- src/componentes/footer/footer.jsx
+++ src/componentes/footer/footer.jsx
@@ -3,11 +3,10 @@ import './footer.css'
 export function Footer() {
     return (
         <div className="div-footer">
-            <p>Contacto</p>
-            <p>Youtube</p>
-            <p>Discord</p>
-            <p>GitHub</p>
-
+            <a>Contacto</a>
+            <a>Youtube</a>
+            <a>Discord</a>
+            <a>GitHub</a>
         </div>
     )
 }
