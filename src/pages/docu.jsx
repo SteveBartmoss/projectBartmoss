ae455564d6c77d7f6b224db9562048078fdcc412
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-07-21 09:15:30 -0600

    se agrego contenido

--- src/pages/docu.jsx
+++ src/pages/docu.jsx
@@ -1,10 +1,21 @@
+import { Card, CardAccions, CardLink, CardText, CardTitle } from "../componentes/card/card";
 import { DivSection } from "../componentes/contenedores/contenedores";
 
 export function Docu() {
     return (
         <div>
             <DivSection>
-                <h1>Docu</h1>
+                <Card variant="card-outlined">
+                    <CardTitle>Primeros Pasos React</CardTitle>
+                    <CardText>
+                        Tus primeros pasos en la libreria de 
+                        react pueden ser algo complejos, pero 
+                        aqui te contamos cosas que te pueden ayudar
+                    </CardText>
+                    <CardAccions>
+                        <CardLink url={''} text={'Ir al articulo'} />
+                    </CardAccions>
+                </Card>
             </DivSection>
         </div>
     )
