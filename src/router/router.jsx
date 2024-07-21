1eb311101c8084cfbe5dc1e5634e4fd04a84359a
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-07-21 09:23:33 -0600

    se agrego una ruta para los artiuclos de documentacion

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -4,6 +4,7 @@ import { Home } from "../pages/home";
 import { Footer } from "../componentes/footer/footer";
 import { Steve } from "../pages/steve";
 import { Docu } from "../pages/docu";
+import { FirstStepReact } from "../docu/firstStepReact";
 
 
 export function RouterBlog(){
@@ -14,6 +15,7 @@ export function RouterBlog(){
                 <Route path="/" element={<Home />} />
                 <Route path="steve" element={<Steve />} />
                 <Route path="docu" element={<Docu />} />
+                <Route path="docu/primerospasos" element={<FirstStepReact /> } />
             </Routes>
             <Footer />
         </BrowserRouter>
