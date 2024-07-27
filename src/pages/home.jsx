877dfdb8a4218fddfa12eb8e3a85bbe84f2e9475
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-07-27 12:36:12 -0600

    se cambio la img de perfil

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -4,7 +4,7 @@ import { Portada } from "../componentes/portada/portada";
 export function Home() {
     return (
         <div>
-            <Portada imgPortada={"/Assets/steveBartmoss.jpg"} title={"Void Return"} 
+            <Portada imgPortada={"/Assets/steveA.png"} title={"Void Return"} 
                 text={"Blog dedicado a contar la vida de un programador y algunas otras cosas interesates"} />
             <DivSection>
                 <h1>Ultimos Articulos</h1>
