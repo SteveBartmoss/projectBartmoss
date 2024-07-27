16ff62a57ea2246763745fe40c6872fa7267d4ae
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-07-26 20:34:15 -0600

    se agrego un archivo para un post personal

---
+++ src/post/mistBorn1.jsx
@@ -0,0 +1,84 @@
+import { DivArticle, DivSection } from "../componentes/contenedores/contenedores";
+import { Portada } from "../componentes/portada/portada";
+
+
+export function MistBorn1(){
+    return(
+        <>
+            <Portada imgPortada={"/Assets/steveBartmoss.jpg"} title={"El imperio final es waku waku"} text={"Por Steve 26/07/2024"} />
+            <DivSection>
+                <DivArticle>
+                    <p>
+                        Cuando un companero me recomendo leer la saga 
+                        mistborn porque la idea es que seria un mundo en 
+                        que voldemort gano me dejo cierta intriga pues 
+                        yo tambien era fan de harry potter asi que decidi 
+                        darle una oportunidad
+                    </p>
+
+                    <h1>Historia principal</h1>
+
+                    <p>
+                        La historia se lleva acabo en el mundo se scadrial  y 
+                        por la forma en que se construyo todo el metaverso del 
+                        escritor se puede decir que tambien se desarrolla en el sistema 
+                        de scadrial y es que en este mundo existe cierto sistemas de 
+                        poderes ya que la alomancia es una de las habilidades que existen 
+                        y se puede resumir sin hacer tantos spoliler como la habilidad de 
+                        quemar metales para obtener alguna mejora fisica o de otro tipo.
+                    </p>
+
+                    <p>
+                        En este mundo a las personas con las habilidades anteriormente 
+                        descritas se les llama alomantes y aun grupo reducido se les llama 
+                        nacidos de la bruma (sip ahi entra en juego el nombre del libro) ya que 
+                        poseen mas ventajas en este sistema de poderes, el mundo no se describe 
+                        como cualquiera de otros mundos de fantasia pues se habla de que siempre 
+                        cae ceniza, las plantas son marrones y el sol es de un color rojo ademas 
+                        de que por la noche todo se crube de brumas lo cual queda perfecto con la idea 
+                        de los edificios goticos que son descritos en la narracion.
+                    </p>
+
+                    <p>
+                        Todo lo anterior no podia estar completo sin un villano ya que en este 
+                        caso se trata de una historia alternativa en la que se plantea que pasaria 
+                        si el villano gana en la tipica historia de lucha de poderes y asi fue como 
+                        surgio el imperio final, en el que lord legislador gobierna la tierra con 
+                        mano de hierro tanto que es considerado una deidad
+                    </p>
+
+                    <h1>Spoilers</h1>
+
+                    <p>
+                        Las proxima secciones contienen espoilers sobre el imperio final y otros 
+                        libros de brandon sanderson asi que si quieres descubrir por tu propia cuenta 
+                        la historia del libro puedes dejarlo aqui y volver luego. Por lo que en resumen 
+                        recomiendo que leas el imperio final pues considero que es un buen libro y seria 
+                        dificil que te decepcione asi que deberias darle una oportunidad
+                    </p>
+                    
+                    <h1>Kelsier</h1>
+
+                    <p>
+                        Uno de los primeros personajes que se presenta en el libro 
+                        es kelsier que puede que no sea precisamente el porotagonista 
+                        pero para mi lo fue, una forma rapida de describir como es el 
+                        personaje es pensar en un peter pan deprimido por todo lo que 
+                        le ha pasado. El hecho de que el personaje siempre se describe 
+                        con una sonrisa y esta haciendo bromas por lo cual me gustaba 
+                        pensar en el como lo dije antes en una version de peter pan pero 
+                        deprimido o algo efectado ya que se puede decir que el personaje 
+                        usaba el humor para seguir adelante despues de todo
+                    </p>
+
+                    <h1>Luthadel</h1>
+
+                    <p>
+                        La ciudad que se describe en el libro es bastante llamativa pues 
+                        en mi mente la imaginaba como si fuera una ciudad medieval 
+                    </p>
+                </DivArticle>
+            </DivSection>
+        </>
+    )
+}
