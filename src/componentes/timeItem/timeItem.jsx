85a2bd641b42415072f77a2e1ca73a48fbd2b8e1
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-07-28 16:51:19 -0600

    se cambio la estructura y las clases de estilos

--- src/componentes/timeItem/timeItem.jsx
+++ src/componentes/timeItem/timeItem.jsx
@@ -1,16 +1,16 @@
 import './timeItem.css';
 
-export function TimeItem({ fecha, texto }) {
+export function TimeItem({ fecha, texto, autor }) {
     return (
-        <div>
+        <div className='div-time-item'>
             <div className='head-time-item'>
                 <div>
                     <img className='img-time-item' src="/Assets/steveA.png" />
                 </div>
-                <div>
+                <div className='div-date-item'>
+                    <p>{autor}</p>
                     <p>{fecha}</p>
                 </div>
-                
             </div>
             <div>
                 <p>{texto}</p>
