e9aea3e90ab412a94c5540c344581bb336db7e3d
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-08-03 13:03:25 -0600

    se implemento el contexto para poder hacer el cambio en el tema oscuro o claro

--- src/componentes/navbar/navBar.jsx
+++ src/componentes/navbar/navBar.jsx
@@ -1,3 +1,4 @@
+import { useApp } from '../../context/contextApp';
 import { ThemeBtn } from '../themeBtn/themeBtn';
 import './navBar.css'
 import { NavLink } from "react-router-dom";
@@ -5,15 +6,17 @@ import { NavLink } from "react-router-dom";
 
 export function NavBar() {
 
+    const {isDarkTheme} = useApp()
+
     return (
         <div className="div-nav-bar">
             <div>
-                <NavLink className="nav-item" to={"/"}>Void return</NavLink>
+                <NavLink className= {isDarkTheme ? 'nav-item-light' : 'nav-item-dark'} to={"/"}>Void return</NavLink>
             </div>
             <div>
                 <ThemeBtn />
-                <NavLink className="nav-item" to={"docu"}>Docu</NavLink>
-                <NavLink className="nav-item" to={"steve"}>Steve</NavLink>
+                <NavLink className= {isDarkTheme ? 'nav-item-light' : 'nav-item-dark'} to={"docu"}>Docu</NavLink>
+                <NavLink className= {isDarkTheme ? 'nav-item-light' : 'nav-item-dark'} to={"steve"}>Steve</NavLink>
             </div>
         </div>
     )
