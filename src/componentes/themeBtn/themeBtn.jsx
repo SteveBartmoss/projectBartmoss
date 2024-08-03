2c8242573d0a74efc5a187dc2c6fc0f2158a0d42
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-08-03 12:49:56 -0600

    se agrego la clase de estilos y la importacion de la hoja de stilos

--- src/componentes/themeBtn/themeBtn.jsx
+++ src/componentes/themeBtn/themeBtn.jsx
@@ -1,11 +1,11 @@
 import { useApp } from "../../context/contextApp"
-
+import './themeBtn.css'
 
 export function ThemeBtn(){
 
     const {isDarkTheme, toggleTheme} = useApp()
 
     return(
-        <button onClick={toggleTheme}>{isDarkTheme ? 'on' : 'off'}</button>
+        <button className="btn-theme" onClick={toggleTheme}>{isDarkTheme ? 'on' : 'off'}</button>
     )
 }
