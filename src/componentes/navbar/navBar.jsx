0ae20688975e494c4d4e277cf807e5b39da3f688
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-08-03 11:44:43 -0600

    se limpio el componente de una mala implementacion del contexto

--- src/componentes/navbar/navBar.jsx
+++ src/componentes/navbar/navBar.jsx
@@ -1,4 +1,3 @@
-import { useApp } from '../../context/contextApp';
 import { ThemeBtn } from '../themeBtn/themeBtn';
 import './navBar.css'
 import { NavLink } from "react-router-dom";
@@ -6,15 +5,13 @@ import { NavLink } from "react-router-dom";
 
 export function NavBar() {
 
-    const {isDarkTheme, toggleTheme} = useApp()
-
     return (
         <div className="div-nav-bar">
             <div>
                 <NavLink className="nav-item" to={"/"}>Void return</NavLink>
             </div>
             <div>
-                <ThemeBtn estado={isDarkTheme} accion={toggleTheme} />
+                <ThemeBtn />
                 <NavLink className="nav-item" to={"docu"}>Docu</NavLink>
                 <NavLink className="nav-item" to={"steve"}>Steve</NavLink>
             </div>
