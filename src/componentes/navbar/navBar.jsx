4d214f4ef35e1a32270d549636705b1e49c903e5
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-08-03 11:31:58 -0600

    se implemento el boton para cambiar de tema, se implemento el contexto de la aplicacion

--- src/componentes/navbar/navBar.jsx
+++ src/componentes/navbar/navBar.jsx
@@ -1,14 +1,20 @@
+import { useApp } from '../../context/contextApp';
+import { ThemeBtn } from '../themeBtn/themeBtn';
 import './navBar.css'
 import { NavLink } from "react-router-dom";
 
 
 export function NavBar() {
+
+    const {isDarkTheme, toggleTheme} = useApp()
+
     return (
         <div className="div-nav-bar">
             <div>
                 <NavLink className="nav-item" to={"/"}>Void return</NavLink>
             </div>
             <div>
+                <ThemeBtn estado={isDarkTheme} accion={toggleTheme} />
                 <NavLink className="nav-item" to={"docu"}>Docu</NavLink>
                 <NavLink className="nav-item" to={"steve"}>Steve</NavLink>
             </div>
