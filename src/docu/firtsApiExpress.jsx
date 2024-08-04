384d7e882edc0eb3cadc50dd99526db507f29aa6
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-08-04 16:22:22 -0600

    se agrego un archivo para un articulo sobre express

--- src/docu/firtsApiExpress.jsx
+++ src/docu/firtsApiExpress.jsx
@@ -1,9 +1,21 @@
+import { DivArticle, DivSection } from "../componentes/contenedores/contenedores";
+import { Portada } from "../componentes/portada/portada";
 
 
-export function FirsApiExpress(){
-    return(
+export function FirsApiExpress() {
+    return (
         <>
-            <h1>Hola</h1>
+            <Portada imgPortada={"/Assets/steveA.png"} title={"Primera API con Express"} text={"Por Steve 04/08/2024"} />
+            <DivSection>
+                <DivArticle>
+                    <p>
+                        Express es un framework para backend en node.js 
+                        que permite crear una API de manera sencilla, en este 
+                        arcticulo veremos como crear una de manera simple
+                    </p>
+                </DivArticle>
+            </DivSection>
+
         </>
     )
 }
