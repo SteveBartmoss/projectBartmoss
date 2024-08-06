a0c21208d6d7af35c5ead563803d19b1e6644cec
Tony <89054896+SteveBartmoss@users.noreply.github.com>
2024-08-06 14:19:30 -0600

    Se agrego contenido a la aplacacion

--- src/docu/firtsApiExpress.jsx
+++ src/docu/firtsApiExpress.jsx
@@ -13,6 +13,30 @@ export function FirsApiExpress() {
                         que permite crear una API de manera sencilla, en este 
                         arcticulo veremos como crear una de manera simple
                     </p>
+                    
+                    <h1>Requerimientos</h1>
+
+                    <p>
+                        Node Js instalado <br/>
+                        editor de codigo <br/>
+                        acceso a datos
+                    </p>
+
+                    <h1>Primeros pasos</h1>
+
+                    <p>
+                        Lo primero que tenemos que hacer es inicializar la apliacion 
+                        de node, lo cual podemos hacer con el siguiente comando:
+                    </p>
+
+                    <code>npm init -y</code>
+
+                    <p>
+                        Esto nos inicializa la aplicacion al crear un package.json con 
+                        la estructura basica, si se omite la opcion -y en el comando 
+                        de igual manera se inicializa la aplicacion pero tendremos 
+                        que responder una serie de preguntas
+                    </p>
                 </DivArticle>
             </DivSection>
 
