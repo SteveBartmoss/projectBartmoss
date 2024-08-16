3f75dbfcfb80d8bbd49d6f87e3a49f44eda58fe2
Tony <89054896+SteveBartmoss@users.noreply.github.com>
2024-08-16 08:54:45 -0600

    Create desconectado.jsx
    Se creo un nuevo archivo para un nuevo post para el blog personal

---
+++ src/post/desconectado.jsx
@@ -0,0 +1,29 @@
+
+export function DesconectadoLife(){
+
+  return(
+    <>
+      <p>Desde hace un par de anios deje las redes sociales o al 
+          menos la mayoria ya que si consideramos youtube y whatsapp como 
+          red social entonces no he dejado por completo las redes sociales, 
+pero la mayoria de las aplciaciones populares como facebook, instagram, o X ya 
+no forman parte de mi telefono y tampoco de mi vida cotidiana, y ciertamente 
+se puede notar un cambio pero contrario a lo que puedes creer no todo el cambio 
+fue positivo o me dio poderes y una mentalidad de tiburon como dicen los gurus 
+del emprendimiento, simplementa fue un cambio el cual puede que para mi fue bueno 
+pero para no otros no sea gran cosa.
+
+Una de las primeras cosas que note es que ya no estaba al dia con acontecimientos 
+relevantes o de la cultura popular, que si bien puede sonar mal o como una gran 
+perdida, tampoco es la gran cosa pues principalmente el tipo de cosas que me habia perdido 
+eran en su mayoria memes, eventos canonicos del internet (como el don que compuso una porra para 
+sus pumas, algun chisme de celebridades o cosas por el estilo) noticias de un caracter mas serio 
+como puede ser el conflicto de ucrania, elecciones precidenciales o lo que paso con trump obviamnete 
+llegaron a mi pues solo deje las redes sociales, tampoco es como que me fuera a vivir a una cueva (aunque 
+lo he pensado y no es tan mala idea) asi que la gran cantidad de noticias importantes seguian llegando 
+para mi, pero por otra parte deje de recibir algunas noticias no que eran relevantes o que simplemente 
+eran paja en mi vida cotidiana y debo decir que este fue el cambio mas interesante. 
+      </p>
+    </>
+  )
+}
