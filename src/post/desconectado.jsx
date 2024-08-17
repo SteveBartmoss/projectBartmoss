e65532cb32249293ac34c7ecf3d347e1135cd684
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-08-16 21:18:49 -0600

    se formateo el codigo

--- src/post/desconectado.jsx
+++ src/post/desconectado.jsx
@@ -1,80 +1,80 @@
 import { DivArticle, DivSection } from "../componentes/contenedores/contenedores";
 import { Portada } from "../componentes/portada/portada";
 
-export function DesconectadoLife(){
+export function DesconectadoLife() {
 
-  return(
+  return (
     <>
       <Portada imgPortada={"/Assets/steveA.png"} title={"Vida Desconectado"} text={"Por Steve 16/08/2024"} />
       <DivSection>
         <DivArticle>
           <p>
-            Desde hace un par de anios deje las redes sociales o al 
-          menos la mayoria ya que si consideramos youtube y whatsapp como 
-          red social entonces no he dejado por completo las redes sociales, 
-pero la mayoria de las aplciaciones populares como facebook, instagram, o X ya 
-no forman parte de mi telefono y tampoco de mi vida cotidiana, y ciertamente 
-se puede notar un cambio pero contrario a lo que puedes creer no todo el cambio 
-fue positivo o me dio poderes y una mentalidad de tiburon como dicen los gurus 
-del emprendimiento, simplementa fue un cambio el cual puede que para mi fue bueno 
-pero para no otros no sea gran cosa.
+            Desde hace un par de anios deje las redes sociales o al
+            menos la mayoria ya que si consideramos youtube y whatsapp como
+            red social entonces no he dejado por completo las redes sociales,
+            pero la mayoria de las aplciaciones populares como facebook, instagram, o X ya
+            no forman parte de mi telefono y tampoco de mi vida cotidiana, y ciertamente
+            se puede notar un cambio pero contrario a lo que puedes creer no todo el cambio
+            fue positivo o me dio poderes y una mentalidad de tiburon como dicen los gurus
+            del emprendimiento, simplementa fue un cambio el cual puede que para mi fue bueno
+            pero para no otros no sea gran cosa.
           </p>
 
           <p>
-            Una de las primeras cosas que note es que ya no estaba al dia con acontecimientos 
-relevantes o de la cultura popular, que si bien puede sonar mal o como una gran 
-perdida, tampoco es la gran cosa pues principalmente el tipo de cosas que me habia perdido 
-eran en su mayoria memes, eventos canonicos del internet (como el don que compuso una porra para 
-sus pumas, algun chisme de celebridades o cosas por el estilo) noticias de un caracter mas serio 
-como puede ser el conflicto de ucrania, elecciones precidenciales o lo que paso con trump obviamnete 
-llegaron a mi pues solo deje las redes sociales, tampoco es como que me fuera a vivir a una cueva (aunque 
-lo he pensado y no es tan mala idea) asi que la gran cantidad de noticias importantes seguian llegando 
-para mi, pero por otra parte deje de recibir algunas noticias no que eran relevantes o que simplemente 
-eran paja en mi vida cotidiana y debo decir que este fue el cambio mas interesante.
+            Una de las primeras cosas que note es que ya no estaba al dia con acontecimientos
+            relevantes o de la cultura popular, que si bien puede sonar mal o como una gran
+            perdida, tampoco es la gran cosa pues principalmente el tipo de cosas que me habia perdido
+            eran en su mayoria memes, eventos canonicos del internet (como el don que compuso una porra para
+            sus pumas, algun chisme de celebridades o cosas por el estilo) noticias de un caracter mas serio
+            como puede ser el conflicto de ucrania, elecciones precidenciales o lo que paso con trump obviamnete
+            llegaron a mi pues solo deje las redes sociales, tampoco es como que me fuera a vivir a una cueva (aunque
+            lo he pensado y no es tan mala idea) asi que la gran cantidad de noticias importantes seguian llegando
+            para mi, pero por otra parte deje de recibir algunas noticias no que eran relevantes o que simplemente
+            eran paja en mi vida cotidiana y debo decir que este fue el cambio mas interesante.
           </p>
 
           <p>
-             Esto no lo percibi como que mi dia paso a 25 horas o que magicamente encontre mucho mas tiempo 
-libre porque ya no lo pierdon haciendo scroll, ya que la verdad simplemente cambie un abito por otro u 
-otros, por ejemplo, en lugar de pasar mi tiempo en el transporte publico haciendo scroll en facebook o 
-instagram, simplemente me puse algun leer libro o si podia ver una serie que tenia ganas, de esta manera 
-no es como que mi productividad subio al maximo como te hacen creer, simplmente cambie un pasatiempo 
-por otro, que puede resultar mas sano ? Bueno eso depende de la persona, en mi caso queria leer algunos 
-libros que me recomendaron asi que para mi fue mejor, ademas me logre poner al dia con las temporadas 
-de rick and morty asi que tampoco gane un nobel por esto, pero si pase mas tiempo haciendo algo que me 
-llamaba la atencion.
+            Esto no lo percibi como que mi dia paso a 25 horas o que magicamente encontre mucho mas tiempo
+            libre porque ya no lo pierdon haciendo scroll, ya que la verdad simplemente cambie un abito por otro u
+            otros, por ejemplo, en lugar de pasar mi tiempo en el transporte publico haciendo scroll en facebook o
+            instagram, simplemente me puse algun leer libro o si podia ver una serie que tenia ganas, de esta manera
+            no es como que mi productividad subio al maximo como te hacen creer, simplmente cambie un pasatiempo
+            por otro, que puede resultar mas sano ? Bueno eso depende de la persona, en mi caso queria leer algunos
+            libros que me recomendaron asi que para mi fue mejor, ademas me logre poner al dia con las temporadas
+            de rick and morty asi que tampoco gane un nobel por esto, pero si pase mas tiempo haciendo algo que me
+            llamaba la atencion.
           </p>
 
           <p>
-            En el caso de cuando me encontraba en casa, tampoco es que tuviera mas productividad ya que es mas de lo 
-mismo solo cambie algo por otro ya que en este caso pasaba mas tiempo jugando algun videojuego que me 
-gustara o viendo algo en youtube o incluso trabajando en mis propios proyectos, asi que tal vez si mejorara 
-un poco mi productividad pero no siento que fuera algo del otro mundo como todos lo comentan, per si note algo bueno 
-y es que el volumen de informacion que recivia era menor que en antes y esto dejaba mi cerebro mas relajado 
-ya que no era como antes que sentia que muchos problemas agenos me arruniaban el dia pues una de las razonas por 
-las que deje de usar la redes sociales era porque la nueva moralidad de internet me tenia cansado.
+            En el caso de cuando me encontraba en casa, tampoco es que tuviera mas productividad ya que es mas de lo
+            mismo solo cambie algo por otro ya que en este caso pasaba mas tiempo jugando algun videojuego que me
+            gustara o viendo algo en youtube o incluso trabajando en mis propios proyectos, asi que tal vez si mejorara
+            un poco mi productividad pero no siento que fuera algo del otro mundo como todos lo comentan, per si note algo bueno
+            y es que el volumen de informacion que recivia era menor que en antes y esto dejaba mi cerebro mas relajado
+            ya que no era como antes que sentia que muchos problemas agenos me arruniaban el dia pues una de las razonas por
+            las que deje de usar la redes sociales era porque la nueva moralidad de internet me tenia cansado.
           </p>
 
           <p>
-                    Puede que las demás personas entren a su perfil de Facebook y en la actualizaciones de estado 
-Solo aparezcan memes, publicaciones interesantes o algo que les llame la atención pero en mi caso 
-Me llegaban todo tipo de publicaciones menos de memes, llegaba cansado de la escuela o de mi trabajo en turno 
-Y lo unico que encontraba era publicaciones de feminismo, crímenes de mi ciudad, problemas de tráfico, cosas de derecha 
-Y cosas de izquierda que realmente no me interesaban para nada, no porque no comparta sus ideas si no que 
-Simplemente queria entrar a una red social para olvidarme de los problemas y las miseria de mi vida no para sumergirme mas 
-En eso y fue por esta razón que abandone las redes sociales, ya que estaba arto de tener que ver toda esta información que a 
-Mi no me interesaba ni importaba.
+            Puede que las demás personas entren a su perfil de Facebook y en la actualizaciones de estado
+            Solo aparezcan memes, publicaciones interesantes o algo que les llame la atención pero en mi caso
+            Me llegaban todo tipo de publicaciones menos de memes, llegaba cansado de la escuela o de mi trabajo en turno
+            Y lo unico que encontraba era publicaciones de feminismo, crímenes de mi ciudad, problemas de tráfico, cosas de derecha
+            Y cosas de izquierda que realmente no me interesaban para nada, no porque no comparta sus ideas si no que
+            Simplemente queria entrar a una red social para olvidarme de los problemas y las miseria de mi vida no para sumergirme mas
+            En eso y fue por esta razón que abandone las redes sociales, ya que estaba arto de tener que ver toda esta información que a
+            Mi no me interesaba ni importaba.
           </p>
 
           <p>
-            Actualmente debo decir que me siento mejor al no tener toda esta cantidad de información en mi vida cotidiana pero 
-Al mismo tiempo perdi el poco contacto que tenia con algunos de mi amigos lo cual me puso un poco triste pero no fue tanto 
-Ya que aun tenia acceso a whatssapp así que no estaba del todo incomunicado, aun con todo esto no se si es totalmente 
-Recomendable que la gente empieza a dejar de usar sus redes sociales pues no necesariamente les van a dar resultados positivos 
-En todo los casos y ademas esto no es una especie de recomendación o algún discurso gurú simplemente tenia ganas de compartir 
-Mi experiencia dejando las redes sociales por un tiempo
+            Actualmente debo decir que me siento mejor al no tener toda esta cantidad de información en mi vida cotidiana pero
+            Al mismo tiempo perdi el poco contacto que tenia con algunos de mi amigos lo cual me puso un poco triste pero no fue tanto
+            Ya que aun tenia acceso a whatssapp así que no estaba del todo incomunicado, aun con todo esto no se si es totalmente
+            Recomendable que la gente empieza a dejar de usar sus redes sociales pues no necesariamente les van a dar resultados positivos
+            En todo los casos y ademas esto no es una especie de recomendación o algún discurso gurú simplemente tenia ganas de compartir
+            Mi experiencia dejando las redes sociales por un tiempo
           </p>
-          
+
         </DivArticle>
       </DivSection>
     </>
