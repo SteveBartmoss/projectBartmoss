ae3d0db912e2ab040eb5bfff5bd8847371a5f8da
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-08-17 12:10:56 -0600

    se creo un componente para almacenar los timeitems de la pagina steve

---
+++ src/pages/steveTimeline/steveTimeLine.jsx
@@ -0,0 +1,26 @@
+import { TimeItem } from "../../componentes/timeItem/timeItem";
+
+
+export function SteveTimeLine() {
+    return (
+        <div>
+            <TimeItem fecha="16/08/2024" autor="Steve"
+                texto="Una nueva gpu no arreglara mis problemas 
+                pero seguramente si me haga feliz XD" />
+
+            <TimeItem fecha="13/08/2024" autor="Steve"
+                texto="Los editores de codigo deberian 
+                ser eternos, XD" />
+
+            <TimeItem fecha="03/08/2024" autor="Steve"
+                texto="Wow ya mas de la mitad del 2024, 
+                que rapido pasa el tiempo cuando te va mal XD, 
+                el invierno se acerca" />
+
+            <TimeItem
+                fecha="28/07/2024" autor="Steve"
+                texto="Bueno manetener el sofware no es tan malo, en 
+                algun momento esto tiene que funcionar no?" />
+        </div>
+    )
+}
