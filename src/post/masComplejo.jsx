4da9705628a9b12de51b80f4b8819213f00710bd
Tony <89054896+SteveBartmoss@users.noreply.github.com>
2024-08-21 08:28:36 -0600

    Create masComplejo.jsx
    Se agrego un nuevo archivo para un nuevo post

---
+++ src/post/masComplejo.jsx
@@ -0,0 +1,8 @@
+
+<p>
+  Desde que empece a programar había algunas cosas que no me gustaban o que criticaba por la forma en que 
+  Funcionan, por ejemplo solia decir frases como “si el compilador sabe que falta un punto y como porque no lo 
+  Agrega y ya” o también solo pensar que la respuesta ante los errores que me daba el compilador era muy pobre 
+  O que ni siquiera era buena, bien en ese entonces no tenia idea de como era trabajar en un compilador así que 
+  Solo me quejaba pero fue cuando tome clases de lenguajes y autómatas que entendí porque no todo suele ser tan fácil
+</p>
