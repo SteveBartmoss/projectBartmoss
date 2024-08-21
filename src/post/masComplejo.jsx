fc74887e12a59767c95a1ecdbf7f4fcb3b49aed3
Tony <89054896+SteveBartmoss@users.noreply.github.com>
2024-08-21 08:42:17 -0600

    Update masComplejo.jsx
    Se agrego contenido al nuevo post

--- src/post/masComplejo.jsx
+++ src/post/masComplejo.jsx
@@ -6,3 +6,15 @@
   O que ni siquiera era buena, bien en ese entonces no tenia idea de como era trabajar en un compilador así que 
   Solo me quejaba pero fue cuando tome clases de lenguajes y autómatas que entendí porque no todo suele ser tan fácil
 </p>
+
+<p>
+  Cuando tuve la tarea de hacer una pequeña parte de un compilador me di cuenta de la gran cantidad de problemas a 
+  Resolver si querias tener un escaner y un parser mínimamente decente era bastante elevada, cosas como tener que definir 
+  Una gramatica, adapatar esa gramática a un automata para que el scanner te proporcione tokens, buscar que la gramática sea 
+  LL1 para que se pueda crear una tabla que hará que parser construya un árbol sintactico de manera correcta, leer la tabla de símbolos, hacer el decorado de un tu arbol y luego hacer el código de tres direcciones, todo esto se realizó para una gramatica 
+  Simple que apenas era un calculadora y luego una version de de java mas reducida.
+</p>
+
+<p>
+    Luego de esto valore mas el trabajo de las personas que crea los compiladores pues poco se decide de su trabajo y es cuando menos una de las tareas mas difíciles de resolver, no por nada el libro de compiladores tiene la imagen de un dragon contra un caballero, esta experiencia me dejo la lección de que no siempre se debe insultar la función de un programa o el trabajo de otros ya que si no quieres tener estos fallos o mala experiencia entonces has tu propio programa
+</p>
