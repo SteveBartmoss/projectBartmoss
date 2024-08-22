7b5bcccb47023dbde5333f3d211f87c84876c96c
Tony <89054896+SteveBartmoss@users.noreply.github.com>
2024-08-22 08:38:35 -0600

    Create pilaSinFin.jsx
    Se agrego un nuevo post para el blog personal

---
+++ src/post/pilaSinFin.jsx
@@ -0,0 +1,14 @@
+<p>
+  Cuando empiezas a estudiar temas de programacion no eres capas de 
+  ver todo lo que aun no abarcas sobre el mismo tema y es que de alguna 
+  manera la pila de tecnologias se crece demasiado rapido y las curvas de 
+  aprendizaje no siempre son amigables. Antes de decir enfocarme en el desarrollo 
+  web solo conocia tres lenguajes que eran java, C y C++ los cuales no dominaba ninguno 
+  pero al menos me centraba en aprender java, luego de esto aprendi sobre 
+  bases de datos, un poco de emsamblador por las clases de lenguajes 
+  de inerfaz, luego pase por el horrible mundo de php, llegue al extrano 
+  universo de javascript donde todo existe y no existe a la vez.
+</p>
+
+<p>
+</p>
