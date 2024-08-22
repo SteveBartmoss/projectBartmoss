ca35e489451c243bfd378bad6ee5477726848d89
Tony <89054896+SteveBartmoss@users.noreply.github.com>
2024-08-22 08:51:47 -0600

    Update pilaSinFin.jsx
    Se agrego contenido al post

--- src/post/pilaSinFin.jsx
+++ src/post/pilaSinFin.jsx
@@ -7,8 +7,18 @@
   pero al menos me centraba en aprender java, luego de esto aprendi sobre 
   bases de datos, un poco de emsamblador por las clases de lenguajes 
   de inerfaz, luego pase por el horrible mundo de php, llegue al extrano 
-  universo de javascript donde todo existe y no existe a la vez.
+  universo de javascript donde todo existe y no existe a la vez y finalmente 
+  tuve que aprender python para un proyecto de inteligencia artificial.
 </p>
 
 <p>
+  Si bien solo pase de manera superficial por esto lenguajes si empezo a 
+  preocuparme la cantidad de tecnologias que existian y que yo desconocia 
+  pero todo cambiaria cuando la nacion de la web ataco, si bien habia 
+  aprendido un poco de creacion de paginas web con el uso de htlm y css 
+  esto solo permitia crear paginas estaticas que eran lo mas parecido a 
+  lo que se encontraba en el internet de los 80, asi que tenia que entrar 
+  a otra nueva dimension una en la que todo el mundo daba su aporte pero 
+  los estandares habian desaparecido, tenia que entrar al mundo de los 
+  frameworks.
 </p>
