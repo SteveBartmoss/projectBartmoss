f9a969ecf7642f64aaeef8e9efa6bcb56f60617c
Tony <89054896+SteveBartmoss@users.noreply.github.com>
2024-08-23 12:10:39 -0600

    Update compositor.jsx
    Se agrego contenido al post

--- src/post/compositor.jsx
+++ src/post/compositor.jsx
@@ -1,3 +1,14 @@
 <p>
   Composer es una herramienta de gestión de dependencias para PHP. Facilita la administración de las librerías y paquetes que un proyecto PHP necesita para funcionar. Composer permite declarar las bibliotecas que tu proyecto depende y se encarga de instalarlas y actualizarlas según sea necesario, garantizando que tengas todas las versiones correctas y compatibles.
 </p>
+
+<p>
+  Funcionalidades Principales de Composer
+
+  Gestión de Dependencias:
+
+  Composer descarga e instala automáticamente todas las bibliotecas y paquetes que tu proyecto necesita, según lo especificado en un archivo composer.json.
+
+  Puedes especificar versiones específicas de las dependencias para asegurarte de que todas las dependencias sean compatibles entre sí.
+</p>
+
