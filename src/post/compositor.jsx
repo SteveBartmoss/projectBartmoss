f876eefd6c7467c5ade87013dbccfe8324783672
Tony <89054896+SteveBartmoss@users.noreply.github.com>
2024-08-23 12:15:22 -0600

    Update compositor.jsx
    Se agrego contenido al post

--- src/post/compositor.jsx
+++ src/post/compositor.jsx
@@ -12,3 +12,25 @@
   Puedes especificar versiones específicas de las dependencias para asegurarte de que todas las dependencias sean compatibles entre sí.
 </p>
 
+<p>
+    Autoloading:
+
+    Composer también puede generar automáticamente un script de autoloading, que permite cargar clases de forma automática sin tener que incluir manualmente cada archivo de clase en tu proyecto.
+
+    Esto es posible gracias a las convenciones de PSR-4, que Composer implementa.
+</p>
+
+<p>
+      Versionado Semántico:
+
+      Composer sigue las reglas de versionado semántico (SemVer), lo que permite especificar rangos de versiones para las dependencias. Esto ayuda a evitar problemas de compatibilidad cuando una dependencia se actualiza.
+</p>
+
+<p>
+      Repositorio de Paquetes:
+
+      Composer utiliza un repositorio central llamado Packagist para encontrar y descargar paquetes de PHP. Packagist es el repositorio principal donde los desarrolladores publican sus bibliotecas y herramientas para que otros puedan usarlas.
+</p>
+
+
+
