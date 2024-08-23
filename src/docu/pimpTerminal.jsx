1f4fdf10402f059fc322b51ddb25932d0a32bea2
Tony <89054896+SteveBartmoss@users.noreply.github.com>
2024-08-23 14:31:43 -0600

    Create pimpTerminal.jsx
    Se agrego un nuevo post

---
+++ src/docu/pimpTerminal.jsx
@@ -0,0 +1,12 @@
+<p>
+  Cuando eres una persona que pasa algo de tiempo 
+  mirando la terminal o que tiene que correr comandos 
+  de ves en cuando, entonces te puede interesar 
+  tener una terminal mas llamativa
+</p>
+
+<p>
+  Goght
+
+  
+</p>
