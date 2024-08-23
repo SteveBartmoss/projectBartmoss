44c99551cf0937da59196277a2672fb941a319e8
Tony <89054896+SteveBartmoss@users.noreply.github.com>
2024-08-23 08:08:47 -0600

    Create pretyComits.jsx
    Se creo un nuevo post

---
+++ src/docu/pretyComits.jsx
@@ -0,0 +1,17 @@
+<p>
+  Cuando se trabaja con un proyecto de software ya sea 
+  de manera independiente, colaborando en un proyecto de 
+  codigo abierto o en una empresa es casi seguro que usen 
+  un manejador de versiones como lo es git, lo cual no es 
+  precisamente el tema de este post pero si tiene relacion 
+  y es que los commits pueden ser algo serio
+</p>
+
+<p>
+  Cuando trabajamos en nuestro propio repositorio puede que 
+  no sigamos un orden claro a la hora de hacer commits y 
+  es que realmente los unicos que vemos estos mensajes somos 
+  nosotros mismos, lo cual hace que pensemos que no tiene sentido 
+  organizar los mensajes de los commits pero esto puede cambiar cuando 
+  trabajamos con un grupo de personas
+</p>
