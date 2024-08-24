97b43c7bed46749e87c104dc9c2428f9b05ca686
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-08-24 14:44:17 -0600

    se agrego la estructura del post

--- src/post/pimpTerminal.jsx
+++ src/post/pimpTerminal.jsx
@@ -1,33 +1,48 @@
-<p>
-  Cuando eres una persona que pasa algo de tiempo 
-  mirando la terminal o que tiene que correr comandos 
-  de ves en cuando, entonces te puede interesar 
-  tener una terminal mas llamativa
-</p>
+import { DivArticle, DivSection } from "../componentes/contenedores/contenedores"
+import { Portada } from "../componentes/portada/portada"
 
-<p>
-  Goght
+export function PimpTerminal() {
+  return (
+    <>
+      <Portada imgPortada={"/Assets/steveA.png"} title={"Pimp my Terminal"} text={"Por Steve 23/08/2024"} />
 
-  Este es un programa que nos ayudara a tener un tema llamativo 
-  y sin tener que pasar mucho tiempo configurandolo pues 
-  basta con ir al siguiente sitio web:
-</p>
+      <DivSection>
+        <DivArticle>
+          <p>
+            Cuando eres una persona que pasa algo de tiempo
+            mirando la terminal o que tiene que correr comandos
+            de ves en cuando, entonces te puede interesar
+            tener una terminal mas llamativa
+          </p>
 
-<p>
-  https://gogh-co.github.io/Gogh/
-</p>
+          <p>
+            Goght
 
-<p>
-  Ahi podremos ver todos los esquemas de colores disponnibles 
-  y solo tenemos que elegir los codigos de los temas que queremos 
-  instala y listo
-</p>
+            Este es un programa que nos ayudara a tener un tema llamativo
+            y sin tener que pasar mucho tiempo configurandolo pues
+            basta con ir al siguiente sitio web:
+          </p>
 
-<p>
-  Esto solos nos permite tener un esquema de colores llamativo pero 
-  si queremos algo mejor entonces podemos modificar el promp de nuestro 
-  terminal para que se va mas como si fuera un hacker entonces 
-  podemos modificar el promp del terminal
-</p>
+          <p>
+            https://gogh-co.github.io/Gogh/
+          </p>
 
+          <p>
+            Ahi podremos ver todos los esquemas de colores disponnibles
+            y solo tenemos que elegir los codigos de los temas que queremos
+            instala y listo
+          </p>
+
+          <p>
+            Esto solos nos permite tener un esquema de colores llamativo pero
+            si queremos algo mejor entonces podemos modificar el promp de nuestro
+            terminal para que se va mas como si fuera un hacker entonces
+            podemos modificar el promp del terminal
+          </p>
+
+        </DivArticle>
+      </DivSection>
+    </>
+  )
+}
 
