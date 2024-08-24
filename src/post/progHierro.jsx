f04e678b8d9b24c97decd3673bdfbe1bcce6f99a
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-08-24 09:17:00 -0600

    se agrego un nuevo post

---
+++ src/post/progHierro.jsx
@@ -0,0 +1,26 @@
+<p>
+  Cuando pienso en los viejos programadores me doy cuenta de que 
+  realmente esos eran programadores de hierro y ahora solo existen 
+  programadores que no precisamente soy malos pero no pueden ser 
+  considerados como programadores de hierro
+</p>
+
+<p>
+  Lo se, esto puede sonar un poco exagerado pero si pensamos 
+  un poco en lo que se desarrollaban en esos dias veremos que 
+  esos eran otros tiempos, por ejemplo en ese entonces no habia 
+  una web tan potente como ahora pero en algunas clases se ensenaba 
+  sobre sistemas operativos modificando un mismo sistema operativo 
+  gracias a proyecto como minix, los drivers tenian que ser 
+  creados por el mismo desarrollador pues no existian tantas 
+  librerias como ahora
+</p>
+
+<p>
+  Si querias manejar informacion de una manera eficiente creabas 
+  tu estructura de datos y admnistrabas la memoria del sistema, 
+  la gente creaba sus proyectos para manejar las computadoras de 
+  una forma mas eficiente asi que surgieron programas como ed, ex, 
+  elvis, vi y vim los cuales eran editores para terminal creados 
+  por los mismo desarrolladores
+</p>
