5844c347409280fa155b101176d5171835d65536
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-08-24 09:16:43 -0600

    se agrego un nuevo post

---
+++ src/post/pimpTerminal.jsx
@@ -0,0 +1,33 @@
+<p>
+  Cuando eres una persona que pasa algo de tiempo 
+  mirando la terminal o que tiene que correr comandos 
+  de ves en cuando, entonces te puede interesar 
+  tener una terminal mas llamativa
+</p>
+
+<p>
+  Goght
+
+  Este es un programa que nos ayudara a tener un tema llamativo 
+  y sin tener que pasar mucho tiempo configurandolo pues 
+  basta con ir al siguiente sitio web:
+</p>
+
+<p>
+  https://gogh-co.github.io/Gogh/
+</p>
+
+<p>
+  Ahi podremos ver todos los esquemas de colores disponnibles 
+  y solo tenemos que elegir los codigos de los temas que queremos 
+  instala y listo
+</p>
+
+<p>
+  Esto solos nos permite tener un esquema de colores llamativo pero 
+  si queremos algo mejor entonces podemos modificar el promp de nuestro 
+  terminal para que se va mas como si fuera un hacker entonces 
+  podemos modificar el promp del terminal
+</p>
+
+
