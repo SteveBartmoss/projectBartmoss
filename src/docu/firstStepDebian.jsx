7670cfbf1420666aa7987e53dca5604d1f4230b9
Tony <89054896+SteveBartmoss@users.noreply.github.com>
2024-08-27 14:37:02 -0600

    Create firstStepDebian.jsx
    Se agrego un nuevo post sobre debian

---
+++ src/docu/firstStepDebian.jsx
@@ -0,0 +1,16 @@
+
+<p>
+  Cuando instalas la distribucion de debian 
+  hay ciertas cosas que se deben hacer para 
+  que el uso de esta nueva distribucion sea mas 
+  simple o efectiva.
+</p>
+
+<p>
+  Por defecto debian no agrega tu usuario como 
+  super usuario asi que si queremos hacer la 
+  tipica rutina de sudo apt update y sudo apt upgrade 
+  ya que en este caso nuestro usuario no es esta en el grupo 
+  de root asi que tendremo que usar el comando sudo su para 
+  que poder correr comandos como super usuario
+</p>
