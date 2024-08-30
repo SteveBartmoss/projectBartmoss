226e064e6bc36b46d72b13338dc2734cdc516831
Tony <89054896+SteveBartmoss@users.noreply.github.com>
2024-08-30 14:48:27 -0600

    Update dockerBasic.jsx
    Se agrego contenido al post

--- src/docu/dockerBasic.jsx
+++ src/docu/dockerBasic.jsx
@@ -1,5 +1,12 @@
-<>
+<p>
   Como desarrollador es bueno utilizar herramientas que faciliten el trabajo 
   y una de estas herramientas es docker, que nos permitira configurar 
   ambientes de desarrollo y despliegue del mismo de una forma rapida
-</>
+</p>
+
+<p>
+  Docker es una aplicacion que permite crear maquinas virtuales de 
+  una manera mas simple y eficiente al crear contenedores para 
+  cada maquina que tenemos que implementar, cada uno de los 
+  contenedores esta aislado entre ellos.
+</p>
