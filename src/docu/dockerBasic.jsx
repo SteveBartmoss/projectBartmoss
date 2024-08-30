df29d09d41e1c9f95db24c81f256c5905c99e5dd
Tony <89054896+SteveBartmoss@users.noreply.github.com>
2024-08-30 14:23:43 -0600

    Create dockerBasic.jsx
    Se creo un archivo para un nuevo post

---
+++ src/docu/dockerBasic.jsx
@@ -0,0 +1,5 @@
+<>
+  Como desarrollador es bueno utilizar herramientas que faciliten el trabajo 
+  y una de estas herramientas es docker, que nos permitira configurar 
+  ambientes de desarrollo y despliegue del mismo de una forma rapida
+</>
