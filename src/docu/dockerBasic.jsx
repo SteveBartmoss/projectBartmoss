d98428f8e8bf8519befa833758d0adc220b4d44c
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-08-31 11:34:08 -0600

    Se agrego la estructura del articulo

--- src/docu/dockerBasic.jsx
+++ src/docu/dockerBasic.jsx
@@ -1,27 +1,49 @@
-<p>
-  Como desarrollador es bueno utilizar herramientas que faciliten el trabajo 
-  y una de estas herramientas es docker, que nos permitira configurar 
-  ambientes de desarrollo y despliegue del mismo de una forma rapida
-</p>
+import { DivArticle, DivSection } from "../componentes/contenedores/contenedores"
+import { Portada } from "../componentes/portada/portada"
 
-<p>
-  Docker es una aplicacion que permite crear maquinas virtuales de 
-  una manera mas simple y eficiente al crear contenedores para 
-  cada maquina que tenemos que implementar, cada uno de los 
-  contenedores esta aislado entre ellos.
-</p>
+export function DockerBasic() {
+  return (
+    <>
+      <Portada imgPortada={"/Assets/steveA.png"} title={"Empezar con Docker"} text={"Por Steve 30/08/2024"} />
 
-<p>
-    Estos contenedores son mas eficientes y portatiles que 
-    una maquina virtual pero tambien dependen mas del sistema 
-    operavito que contiene el motor de docker
-</p>
+      <DivSection>
+        <DivArticle>
+          <p>
+            Como desarrollador es bueno utilizar herramientas que faciliten el trabajo
+            y una de estas herramientas es docker, que nos permitira configurar
+            ambientes de desarrollo y despliegue del mismo de una forma rapida
+          </p>
 
-<p>
-    Para hacer la instalacion podemos ir a la documentacion de docker 
-    en el siguiente enlace
-</p>
+          <p>
+            Docker es una aplicacion que permite crear maquinas virtuales de
+            una manera mas simple y eficiente al crear contenedores para
+            cada maquina que tenemos que implementar, cada uno de los
+            contenedores esta aislado entre ellos.
+          </p>
 
-<p>
-  https://docs.docker.com/engine/install/debian/
-</p>
+          <p>
+            Estos contenedores son mas eficientes y portatiles que
+            una maquina virtual pero tambien dependen mas del sistema
+            operavito que contiene el motor de docker
+          </p>
+
+          <p>
+            Para hacer la instalacion podemos ir a la documentacion de docker
+            en el siguiente enlace
+          </p>
+
+          <p>
+            https://docs.docker.com/engine/install/debian/
+          </p>
+
+        </DivArticle>
+      </DivSection>
+    </>
+  )
+}
+
+
+
+
+
+
