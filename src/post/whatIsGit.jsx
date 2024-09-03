301f2dd12de635e8e0bf138dac7a800aa4ccbfaa
Tony <89054896+SteveBartmoss@users.noreply.github.com>
2024-09-03 15:59:43 -0600

    Update whatIsGit.jsx
    Se agrego mas contenido al post

--- src/post/whatIsGit.jsx
+++ src/post/whatIsGit.jsx
@@ -5,3 +5,15 @@
 <p>
   Rastreo de Cambios: Git guarda un historial de todos los cambios realizados en el código fuente. Cada vez que se hace un cambio, se puede "commit" (confirmar) ese cambio con un mensaje que describe lo que se hizo. Esto facilita volver a versiones anteriores del proyecto si es necesario.
 </p>
+
+<p>
+    Trabajo en Rama: Git permite crear ramas (branches) del proyecto, lo que significa que puedes desarrollar una característica nueva en una rama separada sin afectar la versión principal del proyecto. Una vez que la nueva característica está completa y probada, se puede fusionar (merge) de nuevo con la rama principal.
+</p>
+
+<p>
+    Colaboración: Varios desarrolladores pueden trabajar en el mismo proyecto al mismo tiempo sin interferir entre sí. Cada desarrollador puede clonar un repositorio (copia local del proyecto), trabajar en él y luego enviar (push) sus cambios de vuelta al repositorio central.
+</p>
+
+<p>
+    Distribuido: A diferencia de otros sistemas de control de versiones que requieren un servidor central, Git es distribuido, lo que significa que cada desarrollador tiene una copia completa del historial del proyecto. Esto mejora la redundancia y permite trabajar sin conexión.
+</p>
