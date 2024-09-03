848a76d40556cc946d0bc64416aa956f3b7bbdd2
Tony <89054896+SteveBartmoss@users.noreply.github.com>
2024-09-03 15:11:50 -0600

    Create whatIsGit.jsx
    Se agrego un nuevo post para la pagina

---
+++ src/post/whatIsGit.jsx
@@ -0,0 +1,7 @@
+<p>
+  Git es un sistema de control de versiones distribuido que permite a múltiples desarrolladores trabajar en un proyecto de manera colaborativa. Fue creado por Linus Torvalds en 2005, inicialmente para gestionar el desarrollo del kernel de Linux. Git es especialmente útil en proyectos de desarrollo de software porque permite:
+</p>
+
+<p>
+  Rastreo de Cambios: Git guarda un historial de todos los cambios realizados en el código fuente. Cada vez que se hace un cambio, se puede "commit" (confirmar) ese cambio con un mensaje que describe lo que se hizo. Esto facilita volver a versiones anteriores del proyecto si es necesario.
+</p>
