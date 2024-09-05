cc7f8f61f4d8ad24b643ff54fa265277f8518eb5
Tony <89054896+SteveBartmoss@users.noreply.github.com>
2024-09-05 09:03:07 -0600

    Update linkedServer.jsx
    Se agrego mas contenido para el post

--- src/docu/linkedServer.jsx
+++ src/docu/linkedServer.jsx
@@ -2,3 +2,19 @@
 <p>
   Un Linked Server (servidor vinculado) es una funcionalidad en sistemas de bases de datos como SQL Server que permite conectar y consultar datos de bases de datos remotas o externas desde el servidor actual. En esencia, crea un "enlace" entre dos bases de datos que pueden estar en servidores diferentes, incluso si utilizan diferentes motores de bases de datos (SQL Server, MySQL, Oracle, etc.).
 </p>
+
+<p>
+  En SQL Server, un Linked Server permite:
+
+  Ejecutar consultas distribuidas que involucren múltiples bases de datos en diferentes servidores.
+
+  Ejecutar comandos SQL en la base de datos vinculada como si fuera una tabla local.
+
+  Usar procedimientos almacenados y otros objetos de la base de datos remota.
+
+  Interactuar con bases de datos heterogéneas (por ejemplo, SQL Server a MySQL o SQL Server a Oracle).
+</p>
+
+<p>
+  Cuando configuras un Linked Server, estás creando una conexión persistente entre el servidor SQL y otro servidor o base de datos. Esto te permite ejecutar consultas que cruzan servidores, como si los datos estuvieran en el mismo servidor.
+</p>
