952829f4f8f98b8de6bfe7e275cddea8b9405359
Tony <89054896+SteveBartmoss@users.noreply.github.com>
2024-09-06 14:16:13 -0600

    Create VsCodeHack.jsx
    Se creo un nuevo post

---
+++ src/docu/VsCodeHack.jsx
@@ -0,0 +1,11 @@
+
+
+<p>
+  Si usas vsCode, entonces estos atajos te pueden servir
+</p>
+
+<p>
+  cntrl + b permite colapsar la barra de actividad del editor, que es donde se suele mostrar el 
+  explorador de archivos, el estatus de git o diferentes extensiones que tenemos instaladas,
+  esto permite liberar mas espacio visual a la hora de programar.
+</p>
