fbc576dc90f8160b75fd8cb84ab729913a840522
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-09-07 14:22:30 -0600

    Se agrego la estrucutra del articulo

--- src/docu/whatIsGit.jsx
+++ src/docu/whatIsGit.jsx
@@ -1,23 +1,40 @@
-<p>
-  Git es un sistema de control de versiones distribuido que permite a múltiples desarrolladores trabajar en un proyecto de manera colaborativa. Fue creado por Linus Torvalds en 2005, inicialmente para gestionar el desarrollo del kernel de Linux. Git es especialmente útil en proyectos de desarrollo de software porque permite:
-</p>
+import { DivArticle, DivSection } from "../componentes/contenedores/contenedores"
+import { Portada } from "../componentes/portada/portada"
 
-<p>
-  Rastreo de Cambios: Git guarda un historial de todos los cambios realizados en el código fuente. Cada vez que se hace un cambio, se puede "commit" (confirmar) ese cambio con un mensaje que describe lo que se hizo. Esto facilita volver a versiones anteriores del proyecto si es necesario.
-</p>
+export function WhatIsGit() {
+  return (
+    <>
+      <Portada imgPortada={"/Assets/steveA.png"} title={"El hook Context en react"} text={"Por Steve 03/09/2024"} />
 
-<p>
-    Trabajo en Rama: Git permite crear ramas (branches) del proyecto, lo que significa que puedes desarrollar una característica nueva en una rama separada sin afectar la versión principal del proyecto. Una vez que la nueva característica está completa y probada, se puede fusionar (merge) de nuevo con la rama principal.
-</p>
+      <DivSection>
+        <DivArticle>
+          
+          <p>
+            Git es un sistema de control de versiones distribuido que permite a múltiples desarrolladores trabajar en un proyecto de manera colaborativa. Fue creado por Linus Torvalds en 2005, inicialmente para gestionar el desarrollo del kernel de Linux. Git es especialmente útil en proyectos de desarrollo de software porque permite:
+          </p>
 
-<p>
-    Colaboración: Varios desarrolladores pueden trabajar en el mismo proyecto al mismo tiempo sin interferir entre sí. Cada desarrollador puede clonar un repositorio (copia local del proyecto), trabajar en él y luego enviar (push) sus cambios de vuelta al repositorio central.
-</p>
+          <p>
+            Rastreo de Cambios: Git guarda un historial de todos los cambios realizados en el código fuente. Cada vez que se hace un cambio, se puede "commit" (confirmar) ese cambio con un mensaje que describe lo que se hizo. Esto facilita volver a versiones anteriores del proyecto si es necesario.
+          </p>
 
-<p>
-    Distribuido: A diferencia de otros sistemas de control de versiones que requieren un servidor central, Git es distribuido, lo que significa que cada desarrollador tiene una copia completa del historial del proyecto. Esto mejora la redundancia y permite trabajar sin conexión.
-</p>
+          <p>
+            Trabajo en Rama: Git permite crear ramas (branches) del proyecto, lo que significa que puedes desarrollar una característica nueva en una rama separada sin afectar la versión principal del proyecto. Una vez que la nueva característica está completa y probada, se puede fusionar (merge) de nuevo con la rama principal.
+          </p>
 
-<p>
-    Para instalar git en un sistema windows debemos ir al siguiente enlace:
-</p>
+          <p>
+            Colaboración: Varios desarrolladores pueden trabajar en el mismo proyecto al mismo tiempo sin interferir entre sí. Cada desarrollador puede clonar un repositorio (copia local del proyecto), trabajar en él y luego enviar (push) sus cambios de vuelta al repositorio central.
+          </p>
+
+          <p>
+            Distribuido: A diferencia de otros sistemas de control de versiones que requieren un servidor central, Git es distribuido, lo que significa que cada desarrollador tiene una copia completa del historial del proyecto. Esto mejora la redundancia y permite trabajar sin conexión.
+          </p>
+
+          <p>
+            Para instalar git en un sistema windows debemos ir al siguiente enlace:
+          </p>
+
+        </DivArticle>
+      </DivSection>
+    </>
+  )
+}
