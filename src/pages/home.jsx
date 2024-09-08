b423c276087f6974988a8ada2ab0341496c185ce
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-09-08 16:12:04 -0600

    Se agrego una funcion para mostrar la informacion de los post

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -5,39 +5,62 @@ import { Card } from "../componentes/card/card";
 
 export function Home() {
 
-    const [dataDocu,setDataDocu] = useState(null)
+    const [dataDocu, setDataDocu] = useState(null)
+    const [dataPost, setDataPost] = useState(null)
 
-    useEffect(()=>{
+    useEffect(() => {
 
         fetch('/data/docuArticles.json')
-        .then((response)=>response.json())
-        .then((jsonData)=>setDataDocu(jsonData))
-        .catch((error)=>console.log('Error: ',error))
+            .then((response) => response.json())
+            .then((jsonData) => setDataDocu(jsonData))
+            .catch((error) => console.log('Error: ', error))
 
         console.log(dataDocu)
-    },[])
+
+        fetch('/data/stevePost.json')
+            .then((response) => response.json())
+            .then((jsonData) => setDataPost(jsonData))
+            .catch((error) => console.log('Error: ',error))
+
+    }, [])
 
     return (
         <div>
-            <Portada imgPortada={"/Assets/steveA.png"} title={"Void Return"} 
+            <Portada imgPortada={"/Assets/steveA.png"} title={"Void Return"}
                 text={"Blog dedicado a contar la vida de un programador y algunas otras cosas interesates"} />
             <DivSection>
                 <h1>Ultimos Articulos</h1>
                 <p>
                     Lista de articulos XD
                 </p>
-                {
-                    dataDocu && Array.isArray(dataDocu) ? (
-                        dataDocu.map(item=>
-                            <Card>
-                                <h1>{item.titulo}</h1>
-                                <p>{item.descripcion}</p>
-                            </Card>
+                <DivRow>
+                    {
+                        dataDocu && Array.isArray(dataDocu) ? (
+                            dataDocu.map(item =>
+                                <Card>
+                                    <h1>{item.titulo}</h1>
+                                    <p>{item.descripcion}</p>
+                                </Card>
+                            )
+                        ) : (
+                            <p>Cargando informacion ...</p>
                         )
-                    ) : (
-                        <p>Cargando informacion ...</p>
-                    )
-                }
+                    }
+                </DivRow>
+                <DivRow>
+                    {
+                        dataPost && Array.isArray(dataPost) ? (
+                            dataPost.map(item => 
+                                <Card>
+                                    <h1>{item.titulo}</h1>
+                                    <p>{item.descripcion}</p>
+                                </Card>
+                            )
+                        ) : (
+                            <p>Caragando informacion ...</p>
+                        )
+                    }
+                </DivRow>
             </DivSection>
         </div>
     )
