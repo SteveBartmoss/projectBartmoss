267ec68b711a5469e2f0b0ad7008f0417cd87461
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-09-08 14:48:00 -0600

    Se implemento un arreglo para mostrar los artoculos

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,7 +1,22 @@
-import { DivSection } from "../componentes/contenedores/contenedores";
+import { useEffect, useState } from "react";
+import { DivRow, DivSection } from "../componentes/contenedores/contenedores";
 import { Portada } from "../componentes/portada/portada";
+import { Card } from "../componentes/card/card";
 
 export function Home() {
+
+    const [dataDocu,setDataDocu] = useState(null)
+
+    useEffect(()=>{
+
+        fetch('/data/docuArticles.json')
+        .then((response)=>response.json())
+        .then((jsonData)=>setDataDocu(jsonData))
+        .catch((error)=>console.log('Error: ',error))
+
+        console.log(dataDocu)
+    },[])
+
     return (
         <div>
             <Portada imgPortada={"/Assets/steveA.png"} title={"Void Return"} 
@@ -11,6 +26,18 @@ export function Home() {
                 <p>
                     Lista de articulos XD
                 </p>
+                {
+                    dataDocu && Array.isArray(dataDocu) ? (
+                        dataDocu.map(item=>
+                            <Card>
+                                <h1>{item.titulo}</h1>
+                                <p>{item.descripcion}</p>
+                            </Card>
+                        )
+                    ) : (
+                        <p>Cargando informacion ...</p>
+                    )
+                }
             </DivSection>
         </div>
     )
