2fdd3452b5424fb22c1e0f4a00e98fe83029d63a
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-09-13 21:42:01 -0600

    Se agrego mas contenido para el post

--- src/post/sqlStander.jsx
+++ src/post/sqlStander.jsx
@@ -4,7 +4,7 @@ import { DivArticle, DivSection } from "../componentes/contenedores/contenedores
 export function SqlStandar(){
     return(
         <>
-            <Portada imgPortada={"/Assets/steveA.png"} title={"Comandos de VsCode"} text={"Por Steve 13/09/2024"} />
+            <Portada imgPortada={"/Assets/steveA.png"} title={"Sql no es estandar"} text={"Por Steve 13/09/2024"} />
 
             <DivSection>
                 <DivArticle>
@@ -13,6 +13,14 @@ export function SqlStandar(){
                         la base de datos de mysql ya que era con lo que trabaja en 
                         en ese entonces y tambien porque no sabia que eso era como 
                         otro lenguaje diferente en las bases de datos.
+                    </p>
+
+                    <p>
+                        Y si bien no es del todo cierto puedo decir que tampoco existe 
+                        un estandar y es que las bases de datos relacionales no tienen 
+                        sentido, ya que al aprender mysql por ejemplo pense que solo se 
+                        trababa de ese tipo de base de datos pero luego descubri que 
+                        slq es un estandar pero realmente de estandar no tiene nada
                     </p>
                 </DivArticle>
             </DivSection>
