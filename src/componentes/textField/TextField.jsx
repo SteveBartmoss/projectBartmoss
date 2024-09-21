e25f2fa3be29b4beab24f3d5fcd97f269de354d6
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-09-21 15:52:39 -0600

    Se actualizo la libreria

--- src/componentes/textField/TextField.jsx
+++ src/componentes/textField/TextField.jsx
@@ -1,18 +1,18 @@
 import { useState } from "react";
 import './TextField.css';
 
-export function TextField({label,isPass}) {
+export function TextField({label,isPass,text,action}) {
 
-    const [text,setText] = useState("")
+    /*const [text,setText] = useState("")
 
     const handleChange=(event)=>{
         setText(event.target.value)
-    }
+    }*/
 
     return (
         <>
             <div className="form">
-                <input type={isPass ? 'password' : 'text'} id="prueba" value={text} onChange={handleChange} className="text-field" placeholder='' />
+                <input type={isPass ? 'password' : 'text'} id="prueba" value={text} onChange={(event)=>action(event)} className="text-field" placeholder='' />
                 <label htmlFor="field" className="text-field-label">{label}</label>
             </div>
         </>
