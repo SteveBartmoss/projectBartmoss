b1caa2bab5ff767e31eba12d55072a9266345c4c
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-09-21 15:53:55 -0600

    Se agrego un textfield para la funcion de filtrar los articulos

--- src/pages/docu.jsx
+++ src/pages/docu.jsx
@@ -1,11 +1,18 @@
 import { useEffect, useState } from "react";
 import { Card, CardAccions, CardLink, CardText, CardTitle } from "../componentes/card/card";
 import { Chip } from "../componentes/chips/chip";
-import { DivSection, DivTopicos } from "../componentes/contenedores/contenedores";
+import { DivRow, DivSection, DivTopicos } from "../componentes/contenedores/contenedores";
 import { Portada } from "../componentes/portada/portada";
 import { useApp } from "../context/contextApp";
+import { TextField } from "../componentes/textField/TextField";
 
 export function Docu() {
+
+    const [textSearch,setTextSearch] = useState("")
+
+    const handleChange=(event)=>{
+        setTextSearch(event.target.value)
+    }
 
     const [dataDocu,setDataDocu]=useState(null)
 
@@ -24,6 +31,12 @@ export function Docu() {
                 title={"La guia del internet"}
                 text={"Por Steve Bartmoss"} />
             <DivSection>
+                <DivTopicos>
+                    <DivRow>
+                        <TextField label={"buscar"} isPass={false} text={textSearch} action={handleChange} />
+                        
+                    </DivRow>
+                </DivTopicos>
                 <DivTopicos>
                     {
                         dataDocu && Array.isArray(dataDocu) ? (
