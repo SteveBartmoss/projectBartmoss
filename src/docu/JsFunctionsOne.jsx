8dd7809796a399cdf5c61a22dcde54773503a3f1
Tony <89054896+SteveBartmoss@users.noreply.github.com>
2024-09-26 08:41:22 -0600

    Create JsFunctionsOne.jsx
    Se agrego un nuevo post para javascript

---
+++ src/docu/JsFunctionsOne.jsx
@@ -0,0 +1,9 @@
+
+
+<p>
+  Cuando trabajas en programacion web te 
+  toparas con el lenguaje JavaScript y estas 
+  son algunas de las funciones que mas me han 
+  servido o que uso mas a menudo para resolver problemas 
+  en javascript
+</p>
