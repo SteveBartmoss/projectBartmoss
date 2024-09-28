f33cb05aec850b9c062b66c3a310b07237423814
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-09-28 12:49:06 -0600

    Se agrego el archivo main de la aplicacion

---
+++ src/pages/portfolio/oraculo/yesOrNo.jsx
@@ -0,0 +1,12 @@
+import { DivArticle, DivSection } from "../../../componentes/contenedores/contenedores";
+
+
+export function YesOrNot(){
+    return(
+        <DivSection>
+            <DivArticle>
+                <h1>Yes Or No</h1>
+            </DivArticle>
+        </DivSection>
+    )
+}
