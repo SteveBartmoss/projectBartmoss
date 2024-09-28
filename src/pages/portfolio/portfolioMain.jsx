0bc331100d4ba09f367cb9858ee960b798bf333b
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-09-28 12:36:54 -0600

    Se agrego contenido para el apartado de portfolio

--- src/pages/portfolio/portfolioMain.jsx
+++ src/pages/portfolio/portfolioMain.jsx
@@ -1,8 +1,21 @@
+import { DivSection, DivTopicos } from "../../componentes/contenedores/contenedores";
 
 
 
-export function PortFolio(){
-    return(
-        <h1>Portfolio</h1>
+export function PortFolio() {
+    return (
+        <DivSection>
+            <DivTopicos>
+                
+                <h1>Portfolio</h1>
+
+                <p>
+                    Puedes ver los proyectos web o de programacion 
+                    que tengo en progreso y quizas encuentras 
+                    algo que te interesa
+                </p>
+            </DivTopicos>
+        </DivSection>
+
     )
 }
