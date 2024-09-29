1293500c18848c5cc83e538d9cd993125d6bc3ee
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-09-29 13:06:36 -0600

    Se agrego el text field para la aplicacion de yes or not

--- src/pages/portfolio/oraculo/yesOrNo.jsx
+++ src/pages/portfolio/oraculo/yesOrNo.jsx
@@ -1,11 +1,23 @@
+import { useState } from "react";
 import { DivArticle, DivSection } from "../../../componentes/contenedores/contenedores";
+import { TextField } from "../../../componentes/textField/TextField";
 
 
 export function YesOrNot(){
+
+    const [question,setQuestion] = useState("")
+
+    const handleChange=(event)=>{
+        setQuestion(event.target.value)
+    }
+
+
     return(
         <DivSection>
             <DivArticle>
                 <h1>Yes Or No</h1>
+
+                <TextField label={"Pregunta"} isPass={false} text={question} action={handleChange} /> 
             </DivArticle>
         </DivSection>
     )
