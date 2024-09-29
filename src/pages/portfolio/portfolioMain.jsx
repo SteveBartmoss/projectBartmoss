ce3a080be14d563f12b090de789661d44a5e42ea
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-09-29 13:14:32 -0600

    Se cambiaron los elementos de la pagina de portfolio

--- src/pages/portfolio/portfolioMain.jsx
+++ src/pages/portfolio/portfolioMain.jsx
@@ -1,3 +1,4 @@
+import { Card } from "../../componentes/card/card";
 import { DivSection, DivTopicos } from "../../componentes/contenedores/contenedores";
 
 
@@ -5,15 +6,18 @@ import { DivSection, DivTopicos } from "../../componentes/contenedores/contenedo
 export function PortFolio() {
     return (
         <DivSection>
-            <DivTopicos>
-                
+            <Card>
                 <h1>Portfolio</h1>
 
                 <p>
-                    Puedes ver los proyectos web o de programacion 
-                    que tengo en progreso y quizas encuentras 
+                    Puedes ver los proyectos web o de programacion
+                    que tengo en progreso y quizas encuentras
                     algo que te interesa
                 </p>
+            </Card>
+            
+            <DivTopicos>
+
             </DivTopicos>
         </DivSection>
 
