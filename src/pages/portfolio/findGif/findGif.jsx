1659037cacd7d565ac130ea3ba4387334ff496d8
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-10-13 13:39:48 -0600

    Se agregaron estilos

--- src/pages/portfolio/findGif/findGif.jsx
+++ src/pages/portfolio/findGif/findGif.jsx
@@ -1,12 +1,12 @@
 import { DivArticle, DivSection } from "../../../componentes/contenedores/contenedores";
-
+import './findGif.css'
 
 export function FindGif() {
     return (
         <>
             <DivSection>
                 <DivArticle>
-                    <div>
+                    <div className="div-center">
                         <h1>Find gifs</h1>
                     </div>
                 </DivArticle>
