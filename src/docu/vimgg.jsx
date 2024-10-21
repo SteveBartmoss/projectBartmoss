f217e454d3e96085fb980a1ca522a6c16887a5a4
Tony <89054896+SteveBartmoss@users.noreply.github.com>
2024-10-21 17:40:18 -0600

    Se agrego una archivo para un post sobre vim

---
+++ src/docu/vimgg.jsx
@@ -0,0 +1,32 @@
+
+
+Vimgg
+
+Una guia para los mortales como steve
+
+Empezar a utilizar vim no es un camino facil
+            pero aprender nuevas cosas resulta interesante y ademas te puede dar
+            una nueva herramienta para futuros proyectos o simplemente para poder
+            variar el entorno de trabajo y poder elegir de otras opciones
+
+            ¿Porque aprender vim?
+
+            Aunque puede parecer que este tipo de programas solo perjudica a 
+            los usuarios en lugar de ayudarlos la realidad es que si puede llegar 
+            a ser una buena herramienta para los desarrolladores ya que por ejemplo 
+            si estuvieras trabajando en un servidor al cual solo es accesible desde 
+            ssh, como podemos editar un archivo que se encuentra en el servidor sin 
+            tener la posibilidad de usar un edutor como vscode o sublime text?
+
+            Puede que esta situacion no sea tan creible pero incluso si estuvieramos 
+            en la sala del servidor es comun que el servidor no tenga la pantalla ni siquiera 
+            mucho menos teclado y raton pero en el caso de que estos elementos estuvieran precentes
+            pot alguna razon, muchos servidores ahorran recursos y no tienen entorno 
+            grafico ya que el usuario normalmente no vera esto asi que aun cuando 
+            quisieramos acceder al servidor como una maquina de escritorio, no tenemos 
+            otra opcion mas que usar el terminal.
+
+            En estos casos es donde aprender una herramienta como vim puede ser de 
+            de ayuda.
+
+            ¿Que es vim?
