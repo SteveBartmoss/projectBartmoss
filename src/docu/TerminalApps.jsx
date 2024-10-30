9a56798356993d99686a525b2e8a158a09528c77
Tony <89054896+SteveBartmoss@users.noreply.github.com>
2024-10-30 08:48:05 -0600

    Se agrego un archivo para un nuevo post

---
+++ src/docu/TerminalApps.jsx
@@ -0,0 +1,14 @@
+
+
+Cuando trabajas con una distribucion de linux puede 
+que pasas mas tiempo mirando la terminal, sobre todo si 
+trabajas como programador ya que muchas programas se 
+tienen que probar o depurar en la terminal o simplemente 
+te gusta trabajar mas con la terminal. 
+
+Por esta razon es buena idea tener aplicaciones para 
+terminal que hagan mas simple el trabajo o simplemente 
+para flexear con otros desarrolladores o quien sabe 
+puede que incluso con alguna minita que te pida reiniciar 
+su equipo porque se trabo y tu le instalaste linux.
+
