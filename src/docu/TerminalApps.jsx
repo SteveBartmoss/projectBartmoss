323dc5f9fdc1d4ab2d7e09d0135c474082f1170a
Tony <89054896+SteveBartmoss@users.noreply.github.com>
2024-10-31 08:46:37 -0600

    Se actualizo el contenido del post

--- src/docu/TerminalApps.jsx
+++ src/docu/TerminalApps.jsx
@@ -12,3 +12,19 @@ para flexear con otros desarrolladores o quien sabe
 puede que incluso con alguna minita que te pida reiniciar 
 su equipo porque se trabo y tu le instalaste linux.
 
+Htop
+
+Esta es una herramienta que te permite ver los procesos 
+y estatus de la maquina, es como un administrador de tareas pero 
+implementado en la terminal lo cual lo hace muy util 
+cuando quieres saber el estado del equipo.
+
+Se lo que estas pensando, que si quieres ver el manejo de 
+recursos de tu computadora puedes simplemente abrir el 
+monitor de recursos, lo cual es valido pero es interesante 
+saber de este tipo de herramientas pues existen equipos 
+que no cuentan con interfaz grafica o simplemente 
+te conectaste por ssh y no tiene tantas opciones. 
+
+En estos casos es bueno saber que existen estas opciones 
+ya que nunca se sabe cuando se pueden necesitar. 
