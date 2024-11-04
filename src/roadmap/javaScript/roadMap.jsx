bae449f6945f4bc21550f4fe07b80b94acda3b5a
Tony <89054896+SteveBartmoss@users.noreply.github.com>
2024-11-04 16:32:25 -0600

    Se agrego contenido para el main de js

--- src/roadmap/javaScript/roadMap.jsx
+++ src/roadmap/javaScript/roadMap.jsx
@@ -1,92 +1,105 @@
+import { DivArticle, DivTopicos } from "../../componentes/contenedores/contenedores"
 import { Portada } from "../../componentes/portada/portada"
 
 
-export function RoadMap(){
+export function RoadMap() {
     return (
         <>
             <Portada imgPortada={"/Assets/steveA.png"} title={"Road map Js"} text={"Por Steve 04/11/2024"} />
+
+            <DivArticle>
+                <DivTopicos>
+                    <h1>Objetos</h1>
+
+                    <h2>Dinamismo</h2>
+
+                    <p>
+                        Propiedades y métodos dinámicos: Añadir, modificar o eliminar propiedades y métodos de un objeto en tiempo de ejecución.
+                        Acceso a propiedades: Notación de punto vs. notación de corchetes.
+                        Enumeración de propiedades: Cómo recorrer propiedades de un objeto (`for...in`, `Object.keys`, `Object.values`, `Object.entries`).
+                        Mutabilidad: Cómo los objetos pueden cambiar a lo largo del tiempo y cómo funciona el paso por referencia.
+                    </p>
+                    
+                </DivTopicos>
+            </DivArticle>
         </>
     )
 }
 
-### Objetos
-#### Dinamismo
-- **Propiedades y métodos dinámicos**: Añadir, modificar o eliminar propiedades y métodos de un objeto en tiempo de ejecución.
-- **Acceso a propiedades**: Notación de punto vs. notación de corchetes.
-- **Enumeración de propiedades**: Cómo recorrer propiedades de un objeto (`for...in`, `Object.keys`, `Object.values`, `Object.entries`).
-- **Mutabilidad**: Cómo los objetos pueden cambiar a lo largo del tiempo y cómo funciona el paso por referencia.
+
+
 
 #### Factory Functions
-- **Funciones de fábrica**: Crear funciones que devuelvan objetos para reutilizar la lógica de creación.
-- **Ventajas**: Mejor organización y encapsulamiento en comparación con el uso directo de objetos literales.
-- **Ejemplos**: Crear objetos con propiedades y métodos compartidos, sin el uso de `new`.
+    - ** Funciones de fábrica **: Crear funciones que devuelvan objetos para reutilizar la lógica de creación.
+- ** Ventajas **: Mejor organización y encapsulamiento en comparación con el uso directo de objetos literales.
+- ** Ejemplos **: Crear objetos con propiedades y métodos compartidos, sin el uso de`new`.
 
 #### Constructor Functions
-- **Funciones constructoras**: Uso de funciones con `new` para crear instancias de objetos.
-- **Convenciones**: Nombrado con mayúscula inicial (`PascalCase`) y uso de `this` para propiedades.
-- **Prototipos**: Introducción a cómo las constructor functions pueden utilizar prototipos para métodos compartidos.
+    - ** Funciones constructoras **: Uso de funciones con `new` para crear instancias de objetos.
+- ** Convenciones **: Nombrado con mayúscula inicial(`PascalCase`) y uso de `this` para propiedades.
+- ** Prototipos **: Introducción a cómo las constructor functions pueden utilizar prototipos para métodos compartidos.
 
 #### Atajos Constructores
-- **Clases (ES6)**: La sintaxis de `class` y cómo es un atajo para crear funciones constructoras.
-- **Métodos estáticos y de instancia**: Cómo definir métodos que no necesitan una instancia y cómo se relacionan con la clase.
-- **Herencia**: Conceptos básicos de herencia utilizando `extends`.
+    - ** Clases(ES6) **: La sintaxis de `class` y cómo es un atajo para crear funciones constructoras.
+- ** Métodos estáticos y de instancia **: Cómo definir métodos que no necesitan una instancia y cómo se relacionan con la clase.
+- ** Herencia **: Conceptos básicos de herencia utilizando`extends`.
 
 ---
 
 ### Funciones
 #### Function
-- **Declaración y expresión de funciones**: Diferencias entre `function` y funciones de tipo `function expression`.
-- **Funciones anónimas y nombradas**: Cuándo usar cada tipo y las ventajas de cada una.
-- **Arrow functions**: Cómo y cuándo usar `=>` para simplificar la sintaxis.
-- **Contexto `this`**: Cómo cambia el contexto de `this` en funciones regulares vs. funciones flecha.
+    - ** Declaración y expresión de funciones **: Diferencias entre `function` y funciones de tipo`function expression`.
+- ** Funciones anónimas y nombradas **: Cuándo usar cada tipo y las ventajas de cada una.
+- ** Arrow functions **: Cómo y cuándo usar `=>` para simplificar la sintaxis.
+- ** Contexto`this` **: Cómo cambia el contexto de `this` en funciones regulares vs.funciones flecha.
 
 #### Valor y referencia
-- **Paso por valor**: Cómo los tipos primitivos (números, cadenas) se pasan por valor.
-- **Paso por referencia**: Cómo los objetos, arrays y funciones se pasan por referencia, lo que afecta la mutabilidad.
-- **Inmutabilidad**: Técnicas para hacer que los datos sean inmutables, como el uso de `Object.freeze()`.
+    - ** Paso por valor **: Cómo los tipos primitivos(números, cadenas) se pasan por valor.
+- ** Paso por referencia **: Cómo los objetos, arrays y funciones se pasan por referencia, lo que afecta la mutabilidad.
+- ** Inmutabilidad **: Técnicas para hacer que los datos sean inmutables, como el uso de`Object.freeze()`.
 
 #### Listar propiedades
-- **`Object.keys`**: Devuelve las claves de un objeto en un array.
-- **`Object.values`**: Devuelve los valores de un objeto en un array.
-- **`Object.entries`**: Devuelve un array de pares `[clave, valor]` para cada propiedad.
-- **Enumeración vs. Propiedades propias**: Uso de `hasOwnProperty` y técnicas para evitar propiedades de prototipos.
+    - ** `Object.keys` **: Devuelve las claves de un objeto en un array.
+- ** `Object.values` **: Devuelve los valores de un objeto en un array.
+- ** `Object.entries` **: Devuelve un array de pares `[clave, valor]` para cada propiedad.
+- ** Enumeración vs.Propiedades propias **: Uso de `hasOwnProperty` y técnicas para evitar propiedades de prototipos.
 
 #### Clonando objetos
-- **Shallow Copy vs. Deep Copy**: Diferencias entre copia superficial (usando `Object.assign`) y profunda (con bibliotecas o JSON).
-- **Métodos de clonación**: `Object.assign()`, desestructuración, `JSON.parse(JSON.stringify())`, y bibliotecas externas.
-- **Copias inmutables**: Técnicas para crear copias de objetos para prevenir modificaciones accidentales.
+    - ** Shallow Copy vs.Deep Copy **: Diferencias entre copia superficial(usando`Object.assign`) y profunda(con bibliotecas o JSON).
+- ** Métodos de clonación **: `Object.assign()`, desestructuración, `JSON.parse(JSON.stringify())`, y bibliotecas externas.
+- ** Copias inmutables **: Técnicas para crear copias de objetos para prevenir modificaciones accidentales.
 
-#### Privado vs. Público
-- **Privacidad en objetos**: Uso de closures y símbolos para simular miembros privados.
-- **Accesibilidad**: Definición de propiedades públicas vs. privadas en objetos y clases.
-- **Módulos y encapsulación**: Uso de módulos para limitar el alcance de propiedades y métodos.
+#### Privado vs.Público
+    - ** Privacidad en objetos **: Uso de closures y símbolos para simular miembros privados.
+- ** Accesibilidad **: Definición de propiedades públicas vs.privadas en objetos y clases.
+- ** Módulos y encapsulación **: Uso de módulos para limitar el alcance de propiedades y métodos.
 
 ---
 
 ### Math
-- **Métodos básicos**: `Math.round`, `Math.ceil`, `Math.floor`, `Math.abs`.
-- **Funciones avanzadas**: `Math.random`, `Math.pow`, `Math.sqrt`, `Math.max`, `Math.min`.
-- **Redondeo**: Diferencias entre redondeo estándar y truncado.
-- **Generación de números aleatorios**: Cómo generar números en rangos específicos.
+    - ** Métodos básicos **: `Math.round`, `Math.ceil`, `Math.floor`, `Math.abs`.
+- ** Funciones avanzadas **: `Math.random`, `Math.pow`, `Math.sqrt`, `Math.max`, `Math.min`.
+- ** Redondeo **: Diferencias entre redondeo estándar y truncado.
+- ** Generación de números aleatorios **: Cómo generar números en rangos específicos.
 
 ---
 
 ### Date
-- **Crear fechas**: Uso de `new Date()`, pasando argumentos específicos.
-- **Métodos para obtener y modificar**: `getFullYear`, `getMonth`, `getDate`, `setFullYear`, etc.
-- **Formateo de fechas**: Técnicas para convertir fechas en cadenas con formatos específicos.
-- **Operaciones de fecha**: Restar, sumar y comparar fechas, uso de `Date.now()` para obtener marcas de tiempo.
+    - ** Crear fechas **: Uso de`new Date()`, pasando argumentos específicos.
+- ** Métodos para obtener y modificar **: `getFullYear`, `getMonth`, `getDate`, `setFullYear`, etc.
+- ** Formateo de fechas **: Técnicas para convertir fechas en cadenas con formatos específicos.
+- ** Operaciones de fecha **: Restar, sumar y comparar fechas, uso de `Date.now()` para obtener marcas de tiempo.
 
 ---
 
 ### Strings
 #### Caracteres de escape
-- **Uso de caracteres de escape**: Cómo insertar comillas, saltos de línea (`\n`), tabulaciones (`\t`) y otros caracteres especiales.
-- **Secuencias de escape comunes**: `\'`, `\"`, `\\`, `\u` (para Unicode).
+    - ** Uso de caracteres de escape **: Cómo insertar comillas, saltos de línea(`\n`), tabulaciones(`\t`) y otros caracteres especiales.
+- ** Secuencias de escape comunes **: `\'`, `\"`, `\\`, `\u`(para Unicode).
 
 #### Template Strings
-- **Introducción a las plantillas de cadenas**: Uso de las comillas invertidas (\`\`) para incluir expresiones dinámicas.
-- **Interpolación**: Uso de `${expresión}` para incrustar valores dentro de cadenas.
+    - ** Introducción a las plantillas de cadenas **: Uso de las comillas invertidas(\`\`) para incluir expresiones dinámicas.
+- **Interpolación**: Uso de `${ expresión }` para incrustar valores dentro de cadenas.
 - **Multilínea**: Crear cadenas de varias líneas sin caracteres de escape.
 - **Expresiones complejas en plantillas**: Incluir llamadas a funciones, operaciones matemáticas y lógica simple.
 
@@ -94,7 +107,7 @@ export function RoadMap(){
 
 ### Declaración VS Expresión
 - **Declaración de funciones**: `function myFunction() { ... }`, en qué casos se recomienda su uso.
-- **Expresión de funciones**: `const myFunction = function() { ... }`, ventajas y diferencias respecto a la declaración.
+- **Expresión de funciones**: `const myFunction = function () { ... }`, ventajas y diferencias respecto a la declaración.
 - **Diferencias clave**: `Hoisting`, asignación de funciones como valores y el concepto de funciones anónimas.
 
 ---
@@ -122,7 +135,7 @@ export function RoadMap(){
 
 ---
 
-### Fat Arrow Function (`=>`)
+### Fat Arrow Function (`=> `)
 - **Sintaxis básica**: Introducción a la sintaxis de las arrow functions.
 - **Ventajas**: Simplificación del código, especialmente en funciones cortas y de una línea.
 - **Limitaciones**: Ausencia del objeto `arguments` y del propio `this`.
@@ -162,7 +175,7 @@ export function RoadMap(){
 - **Bloque `try...catch`**: Manejo de excepciones en JavaScript, estructura básica.
 - **`try...catch...finally`**: Uso de `finally` para ejecutar código independientemente de los errores.
 - **Propagación de errores**: Cómo los errores se "propagan" y pueden capturarse en niveles superiores.
-- **Manejo de errores en funciones asincrónicas**: Introducción a `async/await` y cómo usar `try...catch` con promesas.
+- **Manejo de errores en funciones asincrónicas**: Introducción a `async / await ` y cómo usar `try...catch` con promesas.
 
 ---
 
