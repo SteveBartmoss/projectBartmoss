da3ec31b9b3f1458f235ca747d129d78c1056c06
Tony <89054896+SteveBartmoss@users.noreply.github.com>
2024-11-04 16:11:10 -0600

    Se actualizo el contenido de la pagina principal de road map

--- src/pages/mainRoad.jsx
+++ src/pages/mainRoad.jsx
@@ -1,18 +1,24 @@
 import { DivSection, DivTopicos } from "../componentes/contenedores/contenedores";
 
 
-export function MainRoad(){
+export function MainRoad() {
 
 
-    return(
+    return (
         <div>
             <Portada imgPortada={"/Assets/steveA.png"}
-                title={"La guia del propgramador"}
+                title={"La guia del programador"}
                 text={"Por Steve Bartmoss"} />
 
             <DivSection>
                 <DivTopicos>
-                    
+                    <Card>
+                        <h1>JavaScript</h1>
+                        <p>Guia para estudiar JavaScript</p>
+                        <CardAccions>
+                            <CardLink url="" text={'Ir al articulo'} />
+                        </CardAccions>
+                    </Card>
                 </DivTopicos>
             </DivSection>
         </div>
