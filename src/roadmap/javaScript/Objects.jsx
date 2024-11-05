e426072f7a03ae8dd5ce5de54a44750068ad8049
Tony <89054896+SteveBartmoss@users.noreply.github.com>
2024-11-05 10:07:17 -0600

    Se agrego contenido para pagina

--- src/roadmap/javaScript/Objects.jsx
+++ src/roadmap/javaScript/Objects.jsx
@@ -33,7 +33,18 @@ export function MapObjects() {
                 persona.edad = 31; // Cambia el valor de la propiedad 'edad'
                 console.log(persona); // {nombre: "Juan", edad: 31, ocupacion: "Desarrollador" }
             </code>
-            
+
+            <h2>Eliminar propiedades dinámicamente</h2>
+
+            <p>
+                Para eliminar una propiedad, utiliza el operador delete:
+            </p>
+
+            <code>
+                delete persona.edad; // Elimina la propiedad 'edad'
+                console.log(persona); // {nombre: "Juan", ocupacion: "Desarrollador" }
+            </code>
+
         </>
     )
 }
