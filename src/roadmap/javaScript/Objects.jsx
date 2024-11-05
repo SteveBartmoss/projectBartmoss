864e59072c67aaa1b3543333974ee5b39884736e
Tony <89054896+SteveBartmoss@users.noreply.github.com>
2024-11-05 10:05:05 -0600

    Se agrego mas contenid para la pagina de objetos

--- src/roadmap/javaScript/Objects.jsx
+++ src/roadmap/javaScript/Objects.jsx
@@ -1,9 +1,11 @@
 
 
-export function MapObjects(){
-    return(
+export function MapObjects() {
+    return (
         <>
             <h1>Propiedades dinámicas</h1>
+
+            <h2>Añadir propiedades dinámicamente</h2>
 
             <p>
                 En JavaScript, puedes agregar propiedades a un objeto en cualquier momento, incluso después de haberlo creado.
@@ -16,6 +18,22 @@ export function MapObjects(){
                 persona.edad = 30; // Añadiendo la propiedad 'edad' en tiempo de ejecución
                 console.log(persona); // {nombre: "Juan", edad: 30 }
             </code>
+
+            <p>
+                También se puede añadir una propiedad usando corchetes [], lo cual es útil cuando el nombre de la propiedad está almacenado en una variable:
+            </p>
+
+            <h2>Modificar propiedades dinámicamente</h2>
+
+            <p>
+                Modificar una propiedad es tan sencillo como reasignarle un valor:
+            </p>
+
+            <code>
+                persona.edad = 31; // Cambia el valor de la propiedad 'edad'
+                console.log(persona); // {nombre: "Juan", edad: 31, ocupacion: "Desarrollador" }
+            </code>
+            
         </>
     )
 }
