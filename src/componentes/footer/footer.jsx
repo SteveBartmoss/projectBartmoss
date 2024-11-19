8a45e1cbfe19165f225e60ca2f4b11cd31a4ccc2
Tony <89054896+SteveBartmoss@users.noreply.github.com>
2024-11-19 12:19:36 -0600

    se agrego una leyenda de la implementacion de la libreria de ui

--- src/componentes/footer/footer.jsx
+++ src/componentes/footer/footer.jsx
@@ -3,6 +3,7 @@ import './footer.css'
 export function Footer() {
     return (
         <div className="div-footer">
+            <a>Power by UiSteve</a>
             <a>Contacto</a>
             <a>Youtube</a>
             <a>Discord</a>
