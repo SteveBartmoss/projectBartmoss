bf94ca68164f0029cba814baabaeb3cfea6db078
Tony <89054896+SteveBartmoss@users.noreply.github.com>
2024-11-28 11:26:36 -0600

    Se agrego una nueva seccion para el road map de tecnologias front end

---
+++ src/roadmap/frontEnd/roadMapFront.jsx
@@ -0,0 +1,18 @@
+import { DivArticle, DivContent } from "../../componentes/contenedores/contenedores";
+import { Portada } from "../../componentes/portada/portada";
+
+
+export function roadMapFront(){ 
+
+    return (
+        <>
+            <Portada mgPortada={"/Assets/steveA.png"} title={"Road map FrontEnd"} text={"Por Steve 28/11/2024"} />
+
+            <DivArticle>
+                <DivContent>
+                    
+                </DivContent>
+            </DivArticle>
+        </>
+    )
+}
