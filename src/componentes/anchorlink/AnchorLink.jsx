824b455b3aa7e8ebe62c32cd27a98ba36e69f08c
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-11-30 11:36:36 -0600

    Se agrego la logica para el cambio de colores en el componente

--- src/componentes/anchorlink/AnchorLink.jsx
+++ src/componentes/anchorlink/AnchorLink.jsx
@@ -1,9 +1,13 @@
 import { NavLink } from "react-router-dom";
 import './AnchorLink.css';
+import { useApp } from "../../context/contextApp";
 
 export function AnchorLink({url,text,isBlank=false}){
+
+    const {isDarkTheme} = useApp()
+
     return isBlank ? (
-        <a className="anchor-link" rel="noopener noreferrer" target="_blank" href={url}>{text}</a>
+        <a className={isDarkTheme ? 'anchor-link-ligth' : 'anchor-link-dark' } rel="noopener noreferrer" target="_blank" href={url}>{text}</a>
     ) : (
         <NavLink className="anchor-link" to={url}>{text}</NavLink>
     )
