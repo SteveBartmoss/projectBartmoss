32d34d068871586d64725c9331d15a2ced732b17
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-11-30 11:39:14 -0600

    Se implemento el cambio de tema e informacion para enlaces del footer

--- src/componentes/footer/footer.jsx
+++ src/componentes/footer/footer.jsx
@@ -1,13 +1,18 @@
+import { useApp } from '../../context/contextApp'
+import { AnchorLink } from '../anchorlink/AnchorLink'
 import './footer.css'
 
 export function Footer() {
+
+    const {isDarkTheme} = useApp()
+
     return (
         <div className="div-footer">
-            <a>Power by UiSteve</a>
+            <a className={isDarkTheme ? 'footer-link-ligth' : 'footer-link-dark' } rel="noopener noreferrer" target="_blank" href='https://uisteve.netlify.app/'>Power by UiSteve</a>
             <a>Contacto</a>
-            <a>Youtube</a>
+            <a className={isDarkTheme ? 'footer-link-ligth' : 'footer-link-dark' } rel="noopener noreferrer" target="_blank" href='https://www.youtube.com/@stevechambitas2782'>Youtube</a>
             <a>Discord</a>
-            <a className='footer-link-dark' rel="noopener noreferrer" target="_blank" href='https://github.com/SteveBartmoss' >GitHub</a>
+            <a className={isDarkTheme ? 'footer-link-ligth' : 'footer-link-dark' } rel="noopener noreferrer" target="_blank" href='https://github.com/SteveBartmoss' >GitHub</a>
         </div>
     )
 }
