55f11f11751a17914511f1526b2ae8faf7383c7d
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-11-30 13:11:49 -0600

    Se implemento la logica para mostrar un componente

--- src/componentes/codeSpace/CodeSpace.jsx
+++ src/componentes/codeSpace/CodeSpace.jsx
@@ -1,15 +1,30 @@
+import { useEffect, useState } from 'react';
 import './CodeSpace.css';
 
-export function CodeSpace({title,rawCode}){
+export function CodeSpace({title,rawCode=""}){
+
+    const sliceCode=()=>{
+        let slices=rawCode.split('\n')
+        return slices
+    }
+
+    useEffect(()=>{
+        console.log(sliceCode())
+    },[])
+
     return(
         <div className='code-container'>
             <div className="code-space-title">
                 <h1>{title}</h1>
             </div>
             <div className="code-space">
-                <p>
-                    {rawCode}
-                </p>
+                {
+                    sliceCode().map( lineCode => 
+                        <p>
+                            {lineCode}
+                        </p>
+                    )
+                }
             </div>
         </div>
     )
