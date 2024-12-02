cbe642f7ad80e109980826d051c5521b5304d6a1
Tony <89054896+SteveBartmoss@users.noreply.github.com>
2024-12-02 09:04:04 -0600

    Se agrego formato para el post

--- src/docu/llavesIndices.jsx
+++ src/docu/llavesIndices.jsx
@@ -38,35 +38,43 @@ export function FkAndIndex() {
                         Aquí hay algunas razones clave:
                     </p>
 
+                    <h3>Índices</h3>
+
                     <p>
-                        Índices: Cuando se define una llave foránea, generalmente se crea un índice en la columna de la llave foránea.
+                        Cuando se define una llave foránea, generalmente se crea un índice en la columna de la llave foránea.
                         Este índice permite que la base de datos localice rápidamente las filas relacionadas en la tabla referenciada.
                         Los índices aceleran las búsquedas y las uniones (joins) porque reducen el número de filas que la base de datos necesita escanear.
                     </p>
 
+                    <h3>Integridad referencial</h3>
+
                     <p>
-                        Integridad referencial: Las llaves foráneas garantizan que los valores en una columna de una
+                        Las llaves foráneas garantizan que los valores en una columna de una
                         tabla coincidan con los valores en la columna referenciada de otra tabla. Esto permite que la
                         base de datos optimice las consultas, ya que puede hacer ciertas suposiciones sobre la integridad y
                         la estructura de los datos.
                     </p>
 
+                    <h3>Optimización de consultas</h3>
+
                     <p>
-                        Optimización de consultas: Los motores de bases de datos utilizan información sobre las llaves
+                        Los motores de bases de datos utilizan información sobre las llaves
                         foráneas para optimizar los planes de ejecución de las consultas. Saben que ciertas columnas
                         están relacionadas y pueden usar esta información para elegir el mejor método de unión y
                         orden de operaciones.
                     </p>
 
+                    <h3>Cache y almacenamiento</h3>
+
                     <p>
-                        Cache y almacenamiento: Los sistemas de bases de datos modernos utilizan estrategias avanzadas
+                        Los sistemas de bases de datos modernos utilizan estrategias avanzadas
                         de almacenamiento en caché y distribución de datos que pueden beneficiarse del uso de llaves foráneas.
                         La información referencial permite a la base de datos almacenar y acceder a los datos de manera más eficiente.
                     </p>
 
                     <p>
                         Por esta razon es una muy buena practica hacer relaciones entre las tablas mediante el uso de una llave
-                        foranea ya que esta crear al mismo tiempo un indice que mejorara el tiempo de consulta en caso de tener
+                        foranea ya que esta crea al mismo tiempo un indice que mejorara el tiempo de consulta en caso de tener
                         sentencias join, el rendimiento puede ser tan considerable como bajar una consulta de minutos a segundos si
                         se utiliza de forma eficiente los indices o las llaves foraneas.
                     </p>
