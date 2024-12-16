251610243cae97aa823ace5fa5190bf82dca8772
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-12-16 16:45:31 -0600

    Se actualizo la libreria ui

---
+++ src/componentes/modal/Modal.jsx
@@ -0,0 +1,20 @@
+import './Modal.css'
+
+export function Modal({ children, estado, close, persistent=false }) {
+
+    const handleOutsideClick=(e)=>{
+        if(e.target.className === 'overlay' && persistent === false){
+            close();
+        }
+    }
+
+    return (
+        <div className={estado ? 'overlay' : 'modal-close'} onClick={handleOutsideClick}>
+            <div className="modal-content">
+                {children}
+            </div>
+        </div>
+
+    )
+
+}
