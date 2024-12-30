b2a8bdcd3f8f6df28ff3d6d8dc62c67abd8c16fb
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-12-30 17:04:49 -0600

    Se implemento un decorador para los tokens

---
+++ src/componentes/codeSpace/decorator/picaso.js
@@ -0,0 +1,17 @@
+
+const reservedWords=['let','const','function','import','export','default','return', 'from']
+
+export function decorateToken(token){
+    if(reservedWords.includes(token.character)){
+        return 'reserved-word'
+    }
+    if(token.typeToken === 'Letters'&&!reservedWords.includes(token.character)){
+        return 'variable-word'
+    }
+    if(token.typeToken === 'OperatorSet'){
+        return 'operator-set'
+    }
+    if(token.typeToken === 'OperatorAccess'){
+        return 'operator-access'
+    }
+}
