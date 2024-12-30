37b0e2d0c24529ffabd5b098787545690c78b62a
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-12-30 17:01:25 -0600

    Se creo un componente para las pruebas de la version 2 del resalto de sintaxis

---
+++ src/componentes/codeSpace/CodeSpaceV2.jsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from 'react';
+import './CodeSpace.css';
+import { clasificateToken } from './gogh';
+import { processCode } from './saramago';
+
+export function CodeSpaceV2({title,rawCode=""}){
+
+    const sliceCode=()=>{
+        let slices=rawCode.split('\n')
+        return slices
+    }
+
+    const tokenCode=(codeLine)=>{
+        let tokens=codeLine.split(' ')
+        //console.log(tokens)
+        return tokens
+    }
+
+    useEffect(()=>{
+        //console.log(processCode('const persona = { nombre: "Juan" }'))
+        //console.log(processCode(':+=/!-*'))
+        console.log(processCode(rawCode))
+        //console.log(sliceCode())
+    },[])
+
+    return(
+        <div className='code-container'>
+            <div className="code-space-title">
+                <h1>{title}</h1>
+            </div>
+            <div className="code-space">
+                {
+                    sliceCode().map( lineCode => 
+                        <p>
+                            {
+                                tokenCode(lineCode).map(token =>
+                                    <span className={clasificateToken(token)}>{token} </span>
+                                )
+                            }
+                        </p> 
+                    )
+                }
+            </div>
+        </div>
+    )
+}
