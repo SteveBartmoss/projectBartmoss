2c8c2a46dcf62521aa315d35bf8305b4e1de8e01
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-12-30 16:43:35 -0600

    Se implemento la primera version de lexer para el resaltado de sintaxis

---
+++ src/componentes/codeSpace/saramago.js
@@ -0,0 +1,58 @@
+const operators=/([+=\-*/!])/
+const operatorsSet=/([\{\}\[\]\(\)])/ 
+const letters=/([aA-zZ])/;
+const operatorsAccess=/([:\.])/
+
+export function processCode(code){
+
+    let swap=""
+    let listTokens=[]
+
+    //console.log(code)
+    console.log('longitud',code.length)
+
+    for(let ite=0; ite<=code.length; ite++){
+
+        const char = code[ite];
+
+        if(operators.test(char)){
+            listTokens.push({
+                typeToken: 'Operator',
+                character: char
+            })
+        }
+        if(operatorsSet.test(char)){
+            listTokens.push({
+                typeToken: 'OperatorSet',
+                character: code[ite]
+            })
+        }
+        if(operatorsAccess.test(char)){
+            listTokens.push({
+                typeToken: 'OperatorAccess',
+                character: char
+            })
+        }
+        if(letters.test(char)){
+            swap+=char
+            if (ite<code.length){
+
+            }
+            if(!letters.test(code[ite+1])){
+                listTokens.push({
+                    typeToken: 'letters',
+                    character: swap
+                })
+                swap=''
+            }
+        }
+        /*
+        listTokens.push({
+            typeToken: 'unknow',
+            character: code[ite],
+        })
+        */
+    }
+
+    return listTokens
+}
