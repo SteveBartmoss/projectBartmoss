9ed74d8940534bab7220c1c3b37361153413717a
Tony <89054896+SteveBartmoss@users.noreply.github.com>
2025-01-20 08:50:05 -0600

    Se actualizo el time line

--- src/pages/steveTimeline/steveTimeLine.jsx
+++ src/pages/steveTimeline/steveTimeLine.jsx
@@ -4,7 +4,14 @@ import { TimeItem } from "../../componentes/timeItem/timeItem";
 export function SteveTimeLine() {
     return (
         <div>
-            
+
+            <TimeItem fecha="18/01/2025" autor="Steve" texto="
+               Hay un niño en Venezuela que creció como cualquiera
+                Jugando en calles y aceras sin mucha preocupación
+                Es fácil para los niños, que de cualquier forma juegan
+                Sin pensar que el mundo afuera fue creado pa'l león"
+            />
+
             <TimeItem fecha="18/01/2025" autor="Steve" texto="
                 Para eso están los cantantes
                 Para tocar el corazón
@@ -17,7 +24,7 @@ export function SteveTimeLine() {
                 Expresar con tu don
                 Y es por eso que tú debes servir de conexión"
             />
-            
+
 
             <TimeItem fecha="15/01/2025" autor="Steve" texto="
                 Como cuando tú recuerdas una historia que no debe ser contada
@@ -67,32 +74,32 @@ export function SteveTimeLine() {
 
             <TimeItem fecha="26/12/2024" autor="Steve" texto="
                 Supongo que se termino vientos de invierno no ?
-                Deberia seguir sueño de primavera ?" 
+                Deberia seguir sueño de primavera ?"
             />
 
             <TimeItem fecha="25/12/2024" autor="Steve" texto="
-                Bueno, hoy fue un buen dia" 
+                Bueno, hoy fue un buen dia"
             />
 
             <TimeItem fecha="24/12/2024" autor="Steve" texto="
                 Si cada caracter del codigo de un software costara, 
                 quizas los clientes lo pensarian dos veces antes de 
-                pedir tantas cosas XD" 
+                pedir tantas cosas XD"
             />
 
             <TimeItem fecha="23/12/2024" autor="Steve" texto="
                 Un poeta en Venezuela canta con la frente arriba
-                Lo que la razón le diga y lo que dicte el corazón" 
+                Lo que la razón le diga y lo que dicte el corazón"
             />
 
             <TimeItem fecha="22/12/2024" autor="Steve" texto="
                 Hay un tipo en Venezuela que, aunque digan lo que digan
-                Es real como cualquiera que sienta rencor y amor" 
+                Es real como cualquiera que sienta rencor y amor"
             />
 
             <TimeItem fecha="20/12/2024" autor="Steve" texto="
                 Hay un tipo en Venezuela que quiere morir tranquilo
-                Planea meterse un tiro en un banquillo'e callejón" 
+                Planea meterse un tiro en un banquillo'e callejón"
             />
 
             <TimeItem fecha="19/12/2024" autor="Steve" texto="
@@ -116,99 +123,99 @@ export function SteveTimeLine() {
                 pero bueno volvemos a la normalidad, faltan 19 dias 
                 para navidad" />
 
-            <TimeItem fecha="08/11/2024" autor="Steve" 
+            <TimeItem fecha="08/11/2024" autor="Steve"
                 texto="De nuevo viernes, trabajo todo el dia 
                 y entonces despierto"
-                />
+            />
 
             <TimeItem fecha="07/11/2024" autor="Steve"
                 texto="Las brumas aumentan cada vez mas, 
                 podria este ser el fin ?"
-                />
+            />
 
             <TimeItem fecha="06/11/2024" autor="Steve"
                 texto="jajaja, pero eso que tiene que ver con la 
                 navidad"
-                />
+            />
 
             <TimeItem fecha="04/11/2024" autor="Steve"
                 texto="Bueno esta vez no paso tan rapido el tiempo, 
                 pero aun asi, sigue sin ser suficiente"
-                />
+            />
 
             <TimeItem fecha="01/11/2024" autor="Steve"
                 texto="Al fin es viernes, espero esta 
                 vez no se pase tan rapido"
-                />
-                
+            />
+
             <TimeItem fecha="31/10/2024" autor="Steve"
                 texto="Salir al trabajo sin datos es mi pasion, 
                 bueno espero el regreso a casa no sea un problema"
-                />
+            />
 
             <TimeItem fecha="30/10/2024" autor="Steve"
                 texto="Leer ready player one por cuarta vez
                  no me hara rico pero si feliz XD"
-                 />
+            />
 
             <TimeItem fecha="29/10/2024" autor="Steve"
                 texto="Otro dia, otra linea de codigo"
-                />
+            />
 
             <TimeItem fecha="27/10/2024" autor="Steve"
                 texto="Bueno el viaje estubo algo cansado :("
-                />
-                
+            />
+
             <TimeItem fecha="25/10/2024" autor="Steve"
                 texto="Oh no, oh no, las fiestas son aburridas, 
                 mejor que no me festejen"
-                />
-            
+            />
+
             <TimeItem fecha="24/10/2024" autor="Steve"
                 texto="Viernes pero a que costo, bueno al menos 
                 sera un mini puente"
-                />
+            />
 
-            <TimeItem fecha="24/10/2024" autor="Steve" 
+            <TimeItem fecha="24/10/2024" autor="Steve"
                 texto="Como que What Was I Made For originalmente
                 no lleva maullidos"
-                />
+            />
 
             <TimeItem fecha="23/10/2024" autor="Steve"
                 texto="Bueno caminar no siempre es malo pero 
                 seria bueno no tener que cuidarse de los asaltos"
-                />
+            />
 
             <TimeItem fecha="22/10/2024" autor="Steve"
                 texto="Wow que palisa esta recibiendo wordpress, 
                 bueno quizas sea ora de usar sus propias tecnologias"
-                />
+            />
 
             <TimeItem fecha="21/10/2024" autor="Steve"
                 texto="Bueno el futuro no es como el doc 
                 lo penso pero quizas estamos en el futuro malo"
-                />
+            />
 
             <TimeItem fecha="21/10/2024" autor="Steve"
                 texto="Ya solo un mes mas y 23 dias para 
                 que sea navidad, que rapido XD"
-                />
+            />
 
             <TimeItem fecha="18/10/2024" autor="Steve"
                 texto="Al fin es viernes, espero este fin 
                 no se pase tan rapido"
-                />
-                
+            />
+
             <TimeItem fecha="17/10/2024" autor="Steve"
                 texto="Oh no, elon tenia una empresa de ia y 
                 apenas me entero que ya hasta tienen versiones 
-                de su modelo jajaja" 
-                />
+                de su modelo jajaja"
+            />
 
             <TimeItem fecha="16/10/2024" autor="Steve"
                 texto="Bueno las brumas son mis aliados no ?, 
                 al menos eso dacia el buen kelsier"
-                />
+            />
 
             <TimeItem fecha="15/10/2024" autor="Steve"
                 texto="Quizas usar un vps para mis proyectos
@@ -218,12 +225,12 @@ export function SteveTimeLine() {
             <TimeItem fecha="14/10/2024" autor="Steve"
                 texto="Oh no hermano, wordpress esta en 
                 peligro, bueno nunca me gusto ese servicio"
-                />
+            />
 
             <TimeItem fecha="11/10/2024" autor="Steve"
                 texto="Bueno otro dia, otro programa mas XD"
-                />
-                
+            />
+
             <TimeItem fecha="10/10/2024" autor="Steve"
                 texto="whats the matter pumpkin? 
                 (Que te pasa calabaza?)" />
@@ -247,71 +254,71 @@ export function SteveTimeLine() {
 
             <TimeItem fecha="02/10/2024" autor="Steve"
                 texto="Bueno es hora de comprar cartones, 
-                de todas formas ya no tengo nada que perder" 
-                />
+                de todas formas ya no tengo nada que perder"
+            />
 
-            <TimeItem fecha="30/09/2024" autor="Steve" 
+            <TimeItem fecha="30/09/2024" autor="Steve"
                 texto="Bueno otro gobierno termina, 
                 algunos piensan que trae un cambio pero 
                 se cambia un tirano por otro" />
 
-            <TimeItem fecha="26/09/2024" autor="Steve" 
+            <TimeItem fecha="26/09/2024" autor="Steve"
                 texto="Vulnerabilidad en linux de 9.9, 
                 la realidad esta en peligro date prisa 
                 goku" />
 
-            <TimeItem fecha="25/09/2024" autor="Steve" 
+            <TimeItem fecha="25/09/2024" autor="Steve"
                 texto="I cant believe, today was a good 
                 day" />
 
-            <TimeItem fecha="24/09/2024" autor="Steve" 
+            <TimeItem fecha="24/09/2024" autor="Steve"
                 texto="Parecia broma pero los foros si estan 
                 volviendo, que esta pasando doctor" />
-            
+
             <TimeItem fecha="23/09/2024" autor="Steve"
                 texto="El invicto no se va recuperar solo, 
                 arriba la esperanza abuelita" />
-            
-             <TimeItem fecha="20/09/2024" autor="Steve"
+
+            <TimeItem fecha="20/09/2024" autor="Steve"
                 texto="Cielos tanto tiempo desconectado 
                  me hizo perder mi racha, bueno aqui vamos 
                  de nuevo" />
-            
+
             <TimeItem fecha="13/09/2024" autor="Steve"
                 texto="Oh no, oh no hay que tener cuidado 
                 con el sujeto de la mascara de hokey" />
-            
-            <TimeItem fecha="13/09/2024" autor="Steve" 
+
+            <TimeItem fecha="13/09/2024" autor="Steve"
                 texto="Lo de ahora son los foros oiste 
                 viejo" />
-            
+
             <TimeItem fecha="12/09/2024" autor="Steve"
                 texto="Los tramites son demasiado lentos 
                 porque no simplemente hacen todo digital" />
-            
+
             <TimeItem fecha="11/09/2024" autor="Steve"
                 texto="Bueno programar no es lo mejor 
                 que me ha pasado pero podria ser peor" />
-            
-            <TimeItem fecha="10/09/2024" autor="Steve" 
+
+            <TimeItem fecha="10/09/2024" autor="Steve"
                 texto="La vida seria mas facil si 
                 puediera teletransportarme" />
-            
-            <TimeItem fecha="09/09/2024" autor="Steve" 
+
+            <TimeItem fecha="09/09/2024" autor="Steve"
                 texto="Lo logre, consegui una invitacion 
-                para probar deadlockm ahora si viene lo chido" 
-                />
-            
-            <TimeItem fecha="06/09/2024" autor="Steve" 
+                para probar deadlockm ahora si viene lo chido"
+            />
+
+            <TimeItem fecha="06/09/2024" autor="Steve"
                 texto="Oh no hermano, como que el codigo 
                 no se resuelve solo bueno al menos sera 
                 facil de depurar no ?" />
 
-            <TimeItem fecha="21/08/2024" autor="Steve" 
+            <TimeItem fecha="21/08/2024" autor="Steve"
                 texto="Si bueno, igual pensar en hacer un editor 
                 por mi cuenta no fue una idea tan buena, pero el 
                 camino asi es" />
-            
+
             <TimeItem fecha="16/08/2024" autor="Steve"
                 texto="Una nueva gpu no arreglara mis problemas 
                 pero seguramente si me haga feliz XD" />
