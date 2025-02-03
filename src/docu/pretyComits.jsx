845d89d2c65029cd06bbbf991d07b48ae9efe42f
Steve <za19012397@zapopan.tecmm.edu.mx>
2025-02-03 13:35:17 -0600

    Se implemento el treemap de la pagina

--- src/docu/pretyComits.jsx
+++ src/docu/pretyComits.jsx
@@ -1,7 +1,8 @@
 import { CodeSpace } from "../componentes/codeSpace/CodeSpace";
-import { DivArticle, DivContent, DivSection } from "../componentes/contenedores/contenedores"
+import { DivArticle, DivCol, DivContent, DivRow, DivSection } from "../componentes/contenedores/contenedores"
 import { Lista } from "../componentes/lista/Lista"
 import { Portada } from "../componentes/portada/portada"
+import { TreeMap } from "../componentes/treeMap/TreeMap";
 
 export function PretyCommits() {
 
@@ -13,181 +14,211 @@ export function PretyCommits() {
     "footer: (Opcional) Información adicional, como referencias a tickets o breaking changes."
   ];
 
+  const pageMap = [
+    {
+      titulo: 'Conventional Commits',
+      id: 'coventionalComits',
+    }, 
+    {
+      titulo: 'feat', 
+      id: 'feat',
+    },
+    {
+      titulo: 'fix', 
+      id: 'fix',
+    },
+    {
+      titulo: 'docs', 
+      id: 'docs',
+    },
+    {
+      titulo: 'style', 
+      id: 'style',
+    },
+  ]
+
   return (
     <>
       <Portada imgPortada={"/Assets/steveA.png"} title={"Prety Commits"} text={"Por Steve 21/08/2024"} />
 
-      <DivArticle>
-        <DivContent>
+      <DivRow>
 
-          <h1>Mejorar tus commits</h1>
+        <DivCol>
+          <TreeMap titulo={'Mapa del Sitio'} elementos={pageMap} />
+        </DivCol>
 
-          <p>
-            Cuando se trabaja con un proyecto de software ya sea
-            de manera independiente, colaborando en un proyecto de
-            codigo abierto o en una empresa es casi seguro que usen
-            un manejador de versiones como lo es git, lo cual no es
-            precisamente el tema de este post pero si tiene relacion
-            y es que los commits pueden ser algo serio
-          </p>
+        <DivCol>
+          <DivArticle>
+            <DivContent>
 
-          <p>
-            Cuando trabajamos en nuestro propio repositorio puede que
-            no sigamos un orden claro a la hora de hacer commits y
-            es que realmente los unicos que vemos estos mensajes somos
-            nosotros mismos, lo cual hace que pensemos que no tiene sentido
-            organizar los mensajes de los commits pero esto puede cambiar cuando
-            trabajamos con un grupo de personas
-          </p>
+              <h1>Mejorar tus commits</h1>
 
-          <h2>Conventional Commits</h2>
+              <p>
+                Cuando se trabaja con un proyecto de software ya sea
+                de manera independiente, colaborando en un proyecto de
+                codigo abierto o en una empresa es casi seguro que usen
+                un manejador de versiones como lo es git, lo cual no es
+                precisamente el tema de este post pero si tiene relacion
+                y es que los commits pueden ser algo serio
+              </p>
 
-          <p>
-            Conventional Commits es una convención para escribir mensajes de
-            commit de forma estructurada y estandarizada. Esta convención permite
-            a los equipos de desarrollo mantener un historial de commits claro y
-            coherente, facilitando la comprensión del código, el control de versiones
-            y la automatización en procesos como el versionado semántico y la generación
-            de changelogs.
-          </p>
+              <p>
+                Cuando trabajamos en nuestro propio repositorio puede que
+                no sigamos un orden claro a la hora de hacer commits y
+                es que realmente los unicos que vemos estos mensajes somos
+                nosotros mismos, lo cual hace que pensemos que no tiene sentido
+                organizar los mensajes de los commits pero esto puede cambiar cuando
+                trabajamos con un grupo de personas
+              </p>
 
-          <h2>Reglas para Redactar un Buen Commit Usando Conventional Commits</h2>
+              <h2 id="coventionalComits">Conventional Commits</h2>
 
-          <h3>Formato Básico:</h3>
+              <p>
+                Conventional Commits es una convención para escribir mensajes de
+                commit de forma estructurada y estandarizada. Esta convención permite
+                a los equipos de desarrollo mantener un historial de commits claro y
+                coherente, facilitando la comprensión del código, el control de versiones
+                y la automatización en procesos como el versionado semántico y la generación
+                de changelogs.
+              </p>
 
-          <p>
-            Cada commit debe seguir el siguiente formato:
-          </p>
+              <h2>Reglas para Redactar un Buen Commit Usando Conventional Commits</h2>
 
-          <CodeSpace title={"Formato basico"} rawCode={`<type> [optional scope]: <description> 
-          [optional body]
-          [optional footer(s)]`} />
+              <h3>Formato Básico:</h3>
 
+              <p>
+                Cada commit debe seguir el siguiente formato:
+              </p>
 
-          <Lista elementos={listaTipos} />
+              <CodeSpace title={"Formato basico"} rawCode={`<type> [optional scope]: <description> 
+              [optional body]
+              [optional footer(s)]`} />
 
-          <h2>Tipos Comunes de Commits:</h2>
+              <Lista elementos={listaTipos} />
 
-          <h3>feat</h3>
+              <h2>Tipos Comunes de Commits:</h2>
 
-          <p>
-            Introducción de una nueva funcionalidad. 
-          </p>
+              <h3 id="feat">feat</h3>
 
-          <CodeSpace title={'Ejemplo'} rawCode={`feat: add user authentication`} />
+              <p>
+                Introducción de una nueva funcionalidad.
+              </p>
 
-          <h3>fix</h3>
+              <CodeSpace title={'Ejemplo'} rawCode={`feat: add user authentication`} />
 
-          <p>
-            Corrección de un bug.
-          </p>
+              <h3 id="fix">fix</h3>
 
-          <CodeSpace title={'Ejemplo'} rawCode={`fix: resolve issue with user login`} />
+              <p>
+                Corrección de un bug.
+              </p>
 
-          <h3>docs</h3>
+              <CodeSpace title={'Ejemplo'} rawCode={`fix: resolve issue with user login`} />
 
-          <p>
-            Cambios en la documentación.  
-          </p>
+              <h3 id="docs">docs</h3>
 
-          <CodeSpace title={'Ejemplo'} rawCode={`docs: update API documentation for login endpoint`} />
+              <p>
+                Cambios en la documentación.
+              </p>
 
-          <h3>style</h3>
+              <CodeSpace title={'Ejemplo'} rawCode={`docs: update API documentation for login endpoint`} />
 
-          <p>
-            Cambios que no afectan la lógica del código, solo el formato (espacios, comas, etc.).
-          </p>
+              <h3 id='style'>style</h3>
 
-          <CodeSpace title={'Ejemplo'} rawCode={`style: format code with prettier`} />
+              <p>
+                Cambios que no afectan la lógica del código, solo el formato (espacios, comas, etc.).
+              </p>
 
-          <h3>refactor</h3>
+              <CodeSpace title={'Ejemplo'} rawCode={`style: format code with prettier`} />
 
-          <p>
-            Cambio en el código que no añade funcionalidad nueva ni corrige un error.
-          </p>
+              <h3>refactor</h3>
 
-          <CodeSpace title={'Ejemplo'} rawCode={`refactor: optimize authentication flow`} />
+              <p>
+                Cambio en el código que no añade funcionalidad nueva ni corrige un error.
+              </p>
 
-          <h3>test</h3>
+              <CodeSpace title={'Ejemplo'} rawCode={`refactor: optimize authentication flow`} />
 
-          <p>
-            Añadir o modificar pruebas.
-          </p>
+              <h3>test</h3>
 
-          <CodeSpace title={'Ejemplo'} rawCode={`test: add unit tests for login service`} />
+              <p>
+                Añadir o modificar pruebas.
+              </p>
 
-          <h3>chore</h3>
+              <CodeSpace title={'Ejemplo'} rawCode={`test: add unit tests for login service`} />
 
-          <p>
-            Actualización de tareas de construcción, configuración de herramientas, etc.
-          </p>
+              <h3>chore</h3>
 
-          <CodeSpace title={'Ejemplo'} rawCode={`chore: update dependencies`} />
+              <p>
+                Actualización de tareas de construcción, configuración de herramientas, etc.
+              </p>
 
-          <h3>perf</h3>
+              <CodeSpace title={'Ejemplo'} rawCode={`chore: update dependencies`} />
 
-          <p>
-            Mejoras en el rendimiento.
-          </p>
+              <h3>perf</h3>
 
-          <CodeSpace title={'Ejemplo'} rawCode={`perf: improve database query performance`} />
+              <p>
+                Mejoras en el rendimiento.
+              </p>
 
-          <h3>build</h3>
+              <CodeSpace title={'Ejemplo'} rawCode={`perf: improve database query performance`} />
 
-          <p>
-            Cambios que afectan el sistema de construcción o dependencias externas.
-          </p>
+              <h3>build</h3>
 
-          <CodeSpace title={'Ejemplo'} rawCode={`build: update webpack to version 5`} />
+              <p>
+                Cambios que afectan el sistema de construcción o dependencias externas.
+              </p>
 
-          <h3>ci</h3>
+              <CodeSpace title={'Ejemplo'} rawCode={`build: update webpack to version 5`} />
 
-          <p>
-            Cambios en la configuración de integración continua.
-          </p>
+              <h3>ci</h3>
 
-          <CodeSpace title={'Ejemplo'} rawCode={`ci: update GitHub Actions workflow`} />
+              <p>
+                Cambios en la configuración de integración continua.
+              </p>
 
-          <p>
-            Uso del Scope:
-            scope es opcional, pero útil para indicar qué parte del proyecto se vio afectada.
-          </p>
+              <CodeSpace title={'Ejemplo'} rawCode={`ci: update GitHub Actions workflow`} />
 
-          <CodeSpace title={'Ejemplo'} rawCode={`feat(auth): add JWT token verification`} />
+              <p>
+                Uso del Scope:
+                scope es opcional, pero útil para indicar qué parte del proyecto se vio afectada.
+              </p>
 
-          <p>
-            Descripción:
-            La descripción debe ser breve, clara y en tiempo presente.
-          </p>
+              <CodeSpace title={'Ejemplo'} rawCode={`feat(auth): add JWT token verification`} />
 
-          <CodeSpace title={'Ejemplo'} rawCode={`fix: correct validation logic in login form`} />
+              <p>
+                Descripción:
+                La descripción debe ser breve, clara y en tiempo presente.
+              </p>
 
-          <p>
-            Cuerpo del Commit (Opcional):
-            Proporciona detalles adicionales si es necesario.
+              <CodeSpace title={'Ejemplo'} rawCode={`fix: correct validation logic in login form`} />
 
-            fix: correct validation logic in login form
+              <p>
+                Cuerpo del Commit (Opcional):
+                Proporciona detalles adicionales si es necesario.
 
-            The previous logic did not account for edge cases where the user inputs special characters. This change introduces a regex to handle these cases correctly.
-          </p>
+                fix: correct validation logic in login form
 
-          <p>
-            Footer (Opcional):
+                The previous logic did not account for edge cases where the user inputs special characters. This change introduces a regex to handle these cases correctly.
+              </p>
 
-            Se usa para referencias a tickets (por ejemplo, Jira, GitHub Issues) o para indicar cambios importantes.
+              <p>
+                Footer (Opcional):
 
-            BREAKING CHANGE: the login API now requires an email instead of username
+                Se usa para referencias a tickets (por ejemplo, Jira, GitHub Issues) o para indicar cambios importantes.
 
-            Closes #123
-          </p>
+                BREAKING CHANGE: the login API now requires an email instead of username
 
-          <p>
-            Conventional Commits proporciona una manera estándar y estructurada de redactar mensajes de commit, lo que facilita la colaboración, el mantenimiento del código, y la automatización de procesos. Al seguir estas reglas, puedes asegurarte de que los commits sean claros, concisos y útiles para cualquier persona que trabaje en el proyecto.
-          </p>
+                Closes #123
+              </p>
 
+              <p>
+                Conventional Commits proporciona una manera estándar y estructurada de redactar mensajes de commit, lo que facilita la colaboración, el mantenimiento del código, y la automatización de procesos. Al seguir estas reglas, puedes asegurarte de que los commits sean claros, concisos y útiles para cualquier persona que trabaje en el proyecto.
+              </p>
 
-        </DivContent>
-      </DivArticle>
+            </DivContent>
+          </DivArticle>
+        </DivCol>
+      </DivRow>
 
     </>
   )
