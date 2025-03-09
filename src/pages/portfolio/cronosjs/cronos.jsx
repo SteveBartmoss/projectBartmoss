705100edab6c5112d4e29338cbef4495aefc288c
steve <za19012397@zapopan.tecmm.edu.mx>
2025-03-09 14:41:07 -0600

    feat: Se agrego una pagina para mostrar informacion sobre el proyecto de cronosjs

---
+++ src/pages/portfolio/cronosjs/cronos.jsx
@@ -0,0 +1,15 @@
+import { Portada } from "../../../componentes/portada/portada";
+
+
+export function CronosJs(){
+
+
+    return(
+        <di>
+            <Portada imgPortada={"/Assets/steveA.png"}
+                title={"CronosJs"}
+                text={"Libreria para trabajar con fechas"} />
+
+        </di>
+    )
+}
