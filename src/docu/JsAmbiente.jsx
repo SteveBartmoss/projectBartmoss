7be14370cd0e3badffbf6cb89d836ba6039c1b2a
Tony <89054896+SteveBartmoss@users.noreply.github.com>
2025-04-04 16:20:38 -0600

    feat: Se agrego parte del formato para el post

--- src/docu/JsAmbiente.jsx
+++ src/docu/JsAmbiente.jsx
@@ -1,9 +1,21 @@
-
+import { Portada } from "../componentes/portada/portada"
+import { DivArticle, DivCol, DivContent, DivRow } from "../componentes/contenedores/contenedores"
 
 export function JsAmbiente(){
     return(
-        <div>
-            <h1>Ambiente de java script</h1>
-        </div>
+        <>
+            <Portada imgPortada={"/Assets/steveA.png"} title={"Ambiente de desarrollo para js"} text={"Por Steve 04/04/2025"} />
+
+            <DivRow>
+
+                <DivCol>
+                    <DivArticle>
+                        <DivContent>
+
+                        </DivContent>
+                    </DivArticle>
+                </DivCol>
+            </DivRow>
+        </>
     )
 }
