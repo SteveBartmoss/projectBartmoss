76f3eb5ecee42e2922dac35f08073f5d59e8673a
Tony <89054896+SteveBartmoss@users.noreply.github.com>
2025-04-15 09:02:34 -0600

    feat: Se agrego contenido para el post

--- src/docu/fundamentosJs.jsx
+++ src/docu/fundamentosJs.jsx
@@ -1,9 +1,10 @@
-
+import { Portada } from "../componentes/portada/portada"
 
 export function FundamentosJs(){
 
     return(
         <>
+            <Portada imgPortada={"/Assets/steveA.png"} title={"Fundamentos de Js"} text={"Por Steve 15/04/2025"} />
         </>
     )
 
