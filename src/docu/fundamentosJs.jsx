b1b8aa4ba29f749e014e257123270d5c9ab9b28e
Tony <89054896+SteveBartmoss@users.noreply.github.com>
2025-04-15 14:23:01 -0600

    feat: Se agrego contenido para el post

--- src/docu/fundamentosJs.jsx
+++ src/docu/fundamentosJs.jsx
@@ -1,3 +1,5 @@
+import { CodeSpaceV2 } from "../componentes/codeSpace/CodeSpaceV2"
+import { DivArticle, DivCol, DivContent, DivRow } from "../componentes/contenedores/contenedores"
 import { Portada } from "../componentes/portada/portada"
 
 export function FundamentosJs(){
@@ -5,6 +7,37 @@ export function FundamentosJs(){
     return(
         <>
             <Portada imgPortada={"/Assets/steveA.png"} title={"Fundamentos de Js"} text={"Por Steve 15/04/2025"} />
+
+            <DivRow>
+                <DivCol>
+
+                </DivCol>
+                <DivCol>
+                    <DivArticle>
+                        <DivContent>
+
+                            <h1>Arrow Functions (Funciones Flecha)</h1>
+
+                            <h2>Sintaxis Basica</h2>
+
+                            <p>
+                                Las arrow functions son una forma mas corta de escribir funciones en JavaScript.
+                            </p>
+
+                            <CodeSpaceV2 title={"Funcion tradicional"} rawCode={`function suma(a, b) {\n return a + b;\n}`} />
+
+                            <CodeSpaceV2 title={"Arrow function equivalente"} rawCode={`const suma = (a, b) => {\nreturn a + b;\n};`} />
+
+                            <p>
+                                Si el cuerpo es una sola lineam, se puede omitir {} y el return
+                            </p>
+
+                            <CodeSpaceV2 title={"Sin simbolos"} rawCode={`const suma = (a, b) => a + b;`} />
+
+                        </DivContent>
+                    </DivArticle>
+                </DivCol>
+            </DivRow>
         </>
     )
 
