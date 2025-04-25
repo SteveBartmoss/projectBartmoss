5b7139433b5217914e1184fc5134b5dd7af242ac
Tony <89054896+SteveBartmoss@users.noreply.github.com>
2025-04-25 13:07:40 -0600

    feat: Se agrego un nuevo archivo para un nuevo post sobre linux

---
+++ src/post/usoLinux.jsx
@@ -0,0 +1,16 @@
+
+
+export function UsoLinux(){
+    return(
+        <>
+            <p>
+                Por un largo tiempo estuve en el lado oscuro, era usuario de windows y 
+                no tenia idea de lo que era un buen servicio de software, tener un buen manejo 
+                de recursos y general usar un sistema operativo que se preocupa por ser de utilidad.
+                Esto puede sonar muy pesimista o como la tipica charla que un linuxero te da pero luego 
+                de usar el sistema por al menos 3 años puedo decir que tienen razon, en este post hablare 
+                sobre mi experiencia con el sistema operativo del famoso pingüino.
+            </p>
+        </>
+    )
+}
