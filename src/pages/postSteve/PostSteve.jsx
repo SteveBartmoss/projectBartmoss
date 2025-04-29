76a38804f537e73b7e6aa1039d82933b35c78d89
steve <za19012397@zapopan.tecmm.edu.mx>
2025-04-29 14:54:49 -0600

    feat: Se implemento la funcion para leer los post desde el archivo de la carpeta data

--- src/pages/postSteve/PostSteve.jsx
+++ src/pages/postSteve/PostSteve.jsx
@@ -11,60 +11,29 @@ export function PostSteve() {
         fetch('/data/stevePost.json')
             .then((response) => response.json())
             .then((jsonData) => setDataPost(jsonData))
-            .catch((error) => console.log('Error: ',error))
+            .catch((error) => console.log('Error: ', error))
     }, [])
 
     return (
         <>
-            
+
             <DivSection>
+                {
+                    dataPost && Array.isArray(dataPost) ? (
 
-                <Card size={30}>
-                    <h1>Estado Desconectado</h1>
-                    <p>
-                        Dejar la redes fue algo interesante
-                        pero no es como lo pintaban o al menos
-                        no lo fue para mi
-                    </p>
-                    <CardAccions>
-                        <CardLink url={'desconectado'} text={'Ir al articulo'} />
-                    </CardAccions>
-                </Card>
-
-                <Card size={30}>
-                    <h1>Pila sin fin</h1>
-                    <p>
-                        La tecnologia parese no tener
-                        fin y continuamente salen
-                        mas cosas que aprender
-                    </p>
-                    <CardAccions>
-                        <CardLink url={'pilasinfin'} text={'Ir al articulo'} />
-                    </CardAccions>
-                </Card>
-
-                <Card size={30}>
-                    <h1>Haslo tu mismo</h1>
-                    <p>
-                        No te agrada un programa, entonces haslo
-                        tu mismo
-                    </p>
-                    <CardAccions>
-                        <CardLink url={'mascomplejo'} text={'Ir al articulo'} />
-                    </CardAccions>
-                </Card>
-
-                <Card size={30}>
-                    <h1>Mi experiencia usando linux</h1>
-                    <p>
-                        El sistema operativo del pinguino 
-                        es mejor de lo que parece
-                    </p>
-                    <CardAccions>
-                        <CardLink url={'usolinux'} text={'Ir al articulo'} />
-                    </CardAccions>
-                </Card>
-
+                        dataPost.map(item =>
+                            <Card size={30} key={item.titulo}>
+                                <h1>{item.titulo}</h1>
+                                <p>{item.descripcion}</p>
+                                <CardAccions>
+                                    <CardLink url={item.url} text={'Ir al articulo'} />
+                                </CardAccions>
+                            </Card>
+                        )
+                    ) : (
+                        <p>Cargando informacion ... </p>
+                    )
+                }
             </DivSection>
         </>
     )
