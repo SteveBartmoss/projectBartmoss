0349ee769732b02dbfcb8b539d96630c8edb785c
steve <za19012397@zapopan.tecmm.edu.mx>
2025-05-03 16:54:24 -0600

    feat: Se implemento una componente para los post de la pagina

---
+++ src/componentes/postCard/postCard.jsx
@@ -0,0 +1,37 @@
+import { NavLink } from "react-router-dom";
+import { useApp } from "../../context/contextApp"
+
+export function PostCard({children}){
+
+    const {isDarkTheme} = useApp()
+
+    return(
+        <div className={`card-basic ${isDarkTheme ? 'ligth' : 'dark'}`}>
+            {children}
+        </div>
+    )
+}
+
+export function PostCardLink({ url, text, isBlank=false }) {
+    return isBlank ? (
+        <a className="post-card-link" rel="noopener noreferrer" target="_blank" href={url}>{text}</a> 
+    ) : (
+        <NavLink className="post-card-link" to={url}>{text}</NavLink>
+    )
+}
+
+export function PostCardAccions({ children }) {
+    return (
+        <div className="post-card-actions">
+            {children}
+        </div>
+    );
+}
+
+export function PostCardContent({children}){
+    return(
+        <div className="post-card-content">
+            {children}
+        </div>
+    );
+}
