e7e3fbf481e4c862f98ae301de0c1bee01d4087e
steve <za19012397@zapopan.tecmm.edu.mx>
2025-05-05 11:02:13 -0600

    feat: Se implemento un nuevo componente para el nuevo lexer de codigo

---
+++ src/componentes/codeSpace/CodeSpaceV3.jsx
@@ -0,0 +1,27 @@
+import { generateUniqueIdV3 } from "../helpers/uid";
+import { decorateToken } from "./decorator/picaso";
+import { processCodeV2, sliceCodeV2 } from "./lexer/AdaLove";
+
+
+export function CodeSpaceV3({title,rawCode=""}){
+    return(
+        <div className="code-container">
+            <div className="code-space-title">
+                <h1>{title}</h1>
+            </div>
+            <div className="code-space">
+                {
+                    sliceCodeV2(rawCode).map(lineCode => 
+                        <p key={generateUniqueIdV3()}>
+                            {
+                                processCodeV2(lineCode).map(token => 
+                                    <span key={generateUniqueIdV3()} className={decorateToken(token)}>{token.character}</span>
+                                )
+                            }
+                        </p>
+                    )
+                }
+            </div>
+        </div>
+    )
+}
