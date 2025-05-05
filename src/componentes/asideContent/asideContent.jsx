093402f27b3415cdfeaafade1648ec92cc9a3282
steve <za19012397@zapopan.tecmm.edu.mx>
2025-05-05 10:07:34 -0600

    feat: Se agrego la importacion de estilos, se agregaron estilos para algunos componentes

--- src/componentes/asideContent/asideContent.jsx
+++ src/componentes/asideContent/asideContent.jsx
@@ -1,9 +1,32 @@
+import './asideContent.css';
 
-
-export function AsideContent(){
-    return(
-        <div>
+export function AsideContent() {
+    return (
+        <div className='div-aside'>
+            <h1>Post escritos por humanos</h1>
+            <p>Este post no apoya la teoria del internet muerto</p>
             <h1>Tambien te puede interesar</h1>
+
+            <div>
+                <div>
+                    <h2>Pila sin fin</h2>
+                    <p>info Pila</p>
+                    <a>ir al post</a>
+                </div>
+
+                <div>
+                    <h2>Pila sin fin</h2>
+                    <p>info Pila</p>
+                    <a>ir al post</a>
+                </div>
+
+                <div>
+                    <h2>Pila sin fin</h2>
+                    <p>info Pila</p>
+                    <a>ir al post</a>
+                </div>
+
+            </div>
         </div>
     )
 }
