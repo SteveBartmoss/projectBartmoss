2210d773576005857fdf65e741144b1244e48496
steve <za19012397@zapopan.tecmm.edu.mx>
2025-05-05 09:39:45 -0600

    feat: Se implemento la nueva estructura para los post

--- src/post/desconectado.jsx
+++ src/post/desconectado.jsx
@@ -1,4 +1,4 @@
-import { DivArticle, DivContent, DivSection } from "../componentes/contenedores/contenedores";
+import { DivArticle, DivCol, DivContent, DivRow, DivSection } from "../componentes/contenedores/contenedores";
 import { Portada } from "../componentes/portada/portada";
 
 export function DesconectadoLife() {
@@ -7,110 +7,118 @@ export function DesconectadoLife() {
     <>
       <Portada imgPortada={"/Assets/steveA.png"} title={"Vida Desconectado"} text={"Por Steve 16/08/2024"} />
 
-      <DivArticle>
-        <DivContent>
-          <p>
-            Hace un par de años dejé las redes sociales, o al menos
-            la mayoría, ya que si consideramos YouTube y WhatsApp como
-            redes sociales, entonces no las he dejado por completo. Sin
-            embargo, las aplicaciones más populares como Facebook, Instagram,
-            o X ya no forman parte de mi teléfono ni de mi vida cotidiana.
-          </p>
+      <DivRow>
+        <DivCol>
 
-          <p>
-            Ciertamente, se nota un cambio, pero, contrario a lo que podrías
-            pensar, no todo fue positivo ni me otorgó "poderes" o una "mentalidad
-            de tiburón", como dicen los gurús del emprendimiento. Simplemente fue
-            un cambio que, para mí, pudo haber sido bueno, pero que para otros puede
-            no significar gran cosa.
-          </p>
+        </DivCol>
+        <DivCol>
+          <DivArticle>
+            <DivContent>
+              <p>
+                Hace un par de años dejé las redes sociales, o al menos
+                la mayoría, ya que si consideramos YouTube y WhatsApp como
+                redes sociales, entonces no las he dejado por completo. Sin
+                embargo, las aplicaciones más populares como Facebook, Instagram,
+                o X ya no forman parte de mi teléfono ni de mi vida cotidiana.
+              </p>
 
-          <h2>Menos informacion basura</h2>
+              <p>
+                Ciertamente, se nota un cambio, pero, contrario a lo que podrías
+                pensar, no todo fue positivo ni me otorgó "poderes" o una "mentalidad
+                de tiburón", como dicen los gurús del emprendimiento. Simplemente fue
+                un cambio que, para mí, pudo haber sido bueno, pero que para otros puede
+                no significar gran cosa.
+              </p>
 
-          <p>
-            Una de las primeras cosas que noté es que ya no estaba al día con
-            acontecimientos relevantes o de la cultura popular. Aunque esto podría
-            sonar mal o como una gran pérdida, en realidad no es algo tan significativo.
-            Principalmente, lo que me perdí fueron, en su mayoría, memes, eventos
-            canónicos de internet (como el hombre que compuso una porra para sus Pumas),
-            algún chisme de celebridades o cosas por el estilo.
-          </p>
+              <h2>Menos informacion basura</h2>
 
-          <p>
-            Noticias de carácter más serio, como el conflicto en Ucrania, las elecciones
-            presidenciales o lo que pasó con Trump, obviamente llegaron a mí. Solo dejé las
-            redes sociales; no es como si me hubiera ido a vivir a una cueva (aunque lo he
-            considerado, y no es tan mala idea). Así que la mayoría de las noticias importantes
-            seguían llegando a mí. Sin embargo, dejé de recibir algunas noticias irrelevantes o
-            que simplemente eran paja en mi vida cotidiana, y debo decir que este fue el cambio
-            más interesante.
-          </p>
+              <p>
+                Una de las primeras cosas que noté es que ya no estaba al día con
+                acontecimientos relevantes o de la cultura popular. Aunque esto podría
+                sonar mal o como una gran pérdida, en realidad no es algo tan significativo.
+                Principalmente, lo que me perdí fueron, en su mayoría, memes, eventos
+                canónicos de internet (como el hombre que compuso una porra para sus Pumas),
+                algún chisme de celebridades o cosas por el estilo.
+              </p>
 
-          <h2>Menos scroll mas tiempo ?</h2>
-          <p>
-            No percibí esto como si mi día hubiese pasado a tener 25 horas o que mágicamente
-            encontré mucho más tiempo libre porque ya no lo perdía haciendo scroll. La verdad
-            es que simplemente cambié un hábito por otro, u otros. Por ejemplo, en lugar de pasar mi
-            tiempo en el transporte público haciendo scroll en Facebook o Instagram, simplemente me
-            puse a leer algún libro o, si podía, ver una serie que tenía ganas de ver.
-          </p>
+              <p>
+                Noticias de carácter más serio, como el conflicto en Ucrania, las elecciones
+                presidenciales o lo que pasó con Trump, obviamente llegaron a mí. Solo dejé las
+                redes sociales; no es como si me hubiera ido a vivir a una cueva (aunque lo he
+                considerado, y no es tan mala idea). Así que la mayoría de las noticias importantes
+                seguían llegando a mí. Sin embargo, dejé de recibir algunas noticias irrelevantes o
+                que simplemente eran paja en mi vida cotidiana, y debo decir que este fue el cambio
+                más interesante.
+              </p>
 
-          <p>
-            De esta manera, no es que mi productividad se disparó como te hacen creer; simplemente
-            cambié un pasatiempo por otro. ¿Puede resultar más sano? Bueno, eso depende de la persona.
-            En mi caso, quería leer algunos libros que me recomendaron, así que para mí fue mejor. Además,
-            logré ponerme al día con las temporadas de Rick and Morty, así que tampoco es que gané un Nobel
-            por esto, pero sí pasé más tiempo haciendo algo que me interesaba.
-          </p>
+              <h2>Menos scroll mas tiempo ?</h2>
 
-          <p>
-            Cuando estaba en casa, tampoco noté un aumento significativo en mi productividad. Fue más de lo
-            mismo: simplemente cambié una cosa por otra. En este caso, pasaba más tiempo jugando algún videojuego
-            que me gustaba, viendo algo en YouTube, o incluso trabajando en mis propios proyectos. Tal vez mi
-            productividad mejoró un poco, pero no siento que fuera algo extraordinario, como muchos lo mencionan.
-          </p>
+              <p>
+                No percibí esto como si mi día hubiese pasado a tener 25 horas o que mágicamente
+                encontré mucho más tiempo libre porque ya no lo perdía haciendo scroll. La verdad
+                es que simplemente cambié un hábito por otro, u otros. Por ejemplo, en lugar de pasar mi
+                tiempo en el transporte público haciendo scroll en Facebook o Instagram, simplemente me
+                puse a leer algún libro o, si podía, ver una serie que tenía ganas de ver.
+              </p>
 
-          <p>
-            Sin embargo, noté algo positivo: el volumen de información que recibía era menor que antes, y esto
-            dejaba mi mente más relajada. Ya no sentía que los problemas ajenos arruinaban mi día, pues una de
-            las razones por las que dejé de usar redes sociales fue porque la nueva moralidad de internet me tenía
-            cansado.
-          </p>
+              <p>
+                De esta manera, no es que mi productividad se disparó como te hacen creer; simplemente
+                cambié un pasatiempo por otro. ¿Puede resultar más sano? Bueno, eso depende de la persona.
+                En mi caso, quería leer algunos libros que me recomendaron, así que para mí fue mejor. Además,
+                logré ponerme al día con las temporadas de Rick and Morty, así que tampoco es que gané un Nobel
+                por esto, pero sí pasé más tiempo haciendo algo que me interesaba.
+              </p>
 
-          <h2>Las redes ya no eran felices</h2>
+              <p>
+                Cuando estaba en casa, tampoco noté un aumento significativo en mi productividad. Fue más de lo
+                mismo: simplemente cambié una cosa por otra. En este caso, pasaba más tiempo jugando algún videojuego
+                que me gustaba, viendo algo en YouTube, o incluso trabajando en mis propios proyectos. Tal vez mi
+                productividad mejoró un poco, pero no siento que fuera algo extraordinario, como muchos lo mencionan.
+              </p>
 
-          <p>
-            Puede que otras personas entren a su perfil de Facebook y en las actualizaciones de estado solo vean
-            memes, publicaciones interesantes o algo que les llame la atención. Pero en mi caso, me llegaban todo
-            tipo de publicaciones, excepto memes. Llegaba cansado de la escuela o de mi trabajo, y lo único que
-            encontraba eran publicaciones sobre feminismo, crímenes en mi ciudad, problemas de tráfico, temas de
-            derecha y de izquierda que realmente no me interesaban.
-          </p>
+              <p>
+                Sin embargo, noté algo positivo: el volumen de información que recibía era menor que antes, y esto
+                dejaba mi mente más relajada. Ya no sentía que los problemas ajenos arruinaban mi día, pues una de
+                las razones por las que dejé de usar redes sociales fue porque la nueva moralidad de internet me tenía
+                cansado.
+              </p>
 
-          <p>
-            No es que no comparta algunas de esas ideas, sino que simplemente quería entrar a una red social para
-            olvidarme de los problemas y la miseria de mi vida, no para sumergirme más en ellos. Fue por esta razón
-            que abandoné las redes sociales: estaba harto de tener que ver toda esa información que no me interesaba
-            ni importaba.
-          </p>
+              <h2>Las redes ya no eran felices</h2>
 
-          <h2>Solo es mi experiencia no un consejo</h2>
+              <p>
+                Puede que otras personas entren a su perfil de Facebook y en las actualizaciones de estado solo vean
+                memes, publicaciones interesantes o algo que les llame la atención. Pero en mi caso, me llegaban todo
+                tipo de publicaciones, excepto memes. Llegaba cansado de la escuela o de mi trabajo, y lo único que
+                encontraba eran publicaciones sobre feminismo, crímenes en mi ciudad, problemas de tráfico, temas de
+                derecha y de izquierda que realmente no me interesaban.
+              </p>
 
-          <p>
-            Actualmente, debo decir que me siento mejor al no tener toda esa cantidad de información en mi vida cotidiana.
-            Sin embargo, al mismo tiempo, perdí el poco contacto que tenía con algunos de mis amigos, lo cual me puso un
-            poco triste. Aun así, no fue tan grave, ya que todavía tenía acceso a WhatsApp, por lo que no estaba completamente
-            incomunicado.
-          </p>
+              <p>
+                No es que no comparta algunas de esas ideas, sino que simplemente quería entrar a una red social para
+                olvidarme de los problemas y la miseria de mi vida, no para sumergirme más en ellos. Fue por esta razón
+                que abandoné las redes sociales: estaba harto de tener que ver toda esa información que no me interesaba
+                ni importaba.
+              </p>
 
-          <p>
-            A pesar de todo esto, no sé si es totalmente recomendable que la gente deje de usar sus redes sociales,
-            pues no necesariamente obtendrán resultados positivos en todos los casos. Además, esto no es una especie de recomendación
-            ni un discurso de gurú; simplemente quería compartir mi experiencia dejando las redes sociales por un tiempo.
-          </p>
+              <h2>Solo es mi experiencia no un consejo</h2>
 
-        </DivContent>
-      </DivArticle>
+              <p>
+                Actualmente, debo decir que me siento mejor al no tener toda esa cantidad de información en mi vida cotidiana.
+                Sin embargo, al mismo tiempo, perdí el poco contacto que tenía con algunos de mis amigos, lo cual me puso un
+                poco triste. Aun así, no fue tan grave, ya que todavía tenía acceso a WhatsApp, por lo que no estaba completamente
+                incomunicado.
+              </p>
+
+              <p>
+                A pesar de todo esto, no sé si es totalmente recomendable que la gente deje de usar sus redes sociales,
+                pues no necesariamente obtendrán resultados positivos en todos los casos. Además, esto no es una especie de recomendación
+                ni un discurso de gurú; simplemente quería compartir mi experiencia dejando las redes sociales por un tiempo.
+              </p>
+
+            </DivContent>
+          </DivArticle>
+        </DivCol>
+      </DivRow>
     </>
   )
 }
