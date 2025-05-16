f05b2ed23905458eb1915ec40c7278eb0078c419
steve <za19012397@zapopan.tecmm.edu.mx>
2025-05-16 14:43:23 -0600

    feat: Se agrego informacion para el contenido del aside de los componentes

--- src/componentes/asideContent/asideContent.jsx
+++ src/componentes/asideContent/asideContent.jsx
@@ -5,6 +5,8 @@ export function AsideContent() {
         <div className='div-aside'>
             <h1>Post escritos por humanos</h1>
             <p>Este post no apoya la teoria del internet muerto</p>
+            <p>Ultima actualizacion</p>
+            <p>Compartir</p>
             <h1>Tambien te puede interesar</h1>
 
             <div>
