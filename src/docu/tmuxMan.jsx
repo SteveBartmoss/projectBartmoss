8ed1083ebc50006801e99484c7f883b13445d695
Marco <za19012397@zapopan.tecmm.edu.mx>
2025-05-27 14:13:17 -0600

    feat: Se agrego contenido para el post sobre tmux

--- src/docu/tmuxMan.jsx
+++ src/docu/tmuxMan.jsx
@@ -26,7 +26,42 @@ export function TmuxMan(){
                                 esta herramienta.
                             </p>
 
+                            <h1>Instalar tmux</h1>
+
+                            <h3>Linux</h3>
+
+                            <p>
+                                Si estamos en linux podemos usar el gestor de paquete la distribucion que usas, a continuacion 
+                                se muestran algunos ejemplos
+                            </p>
+
+                            <h3>Arch linux</h3>
+
+                            <p>pacman -S tmux</p>
+
+                            <h3>Debian o derivados</h3>
+
+                            <p>apt install tmux</p>
+
+                            <h3>Fedora</h3>
+
+                            <p>dnf install tmux</p>
+
+                            <h3>RHEL or CentOS</h3>
+
+                            <p>yum install tmux</p>
                             
+                            <h3>macOS HomeBrew</h3>
+
+                            <p>brew install tmux</p>
+
+                            <h3>macOs macporst</h3>
+
+                            <p>port install tmux</p>
+
+                            <h3>openSUSE</h3>
+
+                            <p>zypper install tmux</p>
 
                         </DivContent>
                     </DivArticle>
