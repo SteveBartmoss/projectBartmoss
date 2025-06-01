2b9caf01b7803bd61c9554f61d5160bda4f034fc
SteveBartmoss <za19012397@zapopan.tecmm.edu.mx>
2025-06-01 16:30:36 -0600

    featr : Se agrego contenido para el archivo sobre primeros pasos en go

--- src/docu/firstStepGo.jsx
+++ src/docu/firstStepGo.jsx
@@ -61,6 +61,67 @@ export function FirstStepGo(){
                                 echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.bashrc
                                 source ~/.bashrc
                             </p>
+
+                            <p>
+                                Esto agrega go a nuestro $PATH y ahora podemos verificar la version de go que tenemos instalado con el siguiente comando
+                            </p>
+
+                            <p>
+                                go version
+                            </p>
+
+                            <p>
+                                Esto nos mostrara la version de go que tenemos instalada y con esto sabremos que todo se instalo correctamente
+                            </p>
+
+                            <h2>Crear el primer programa en go</h2>
+
+                            <p>
+                                Ahora con el entorno configurado podemos crear un progama simple de hola mundo
+                            </p>
+
+                            <p>
+                                package main
+
+                                import "fmt"
+
+                                func main() {
+                                    fmt.Println("¡Hola, Linux Mint!")
+                                }
+                            </p>
+
+                            <p>
+                                Guardamos el archivo como hola.go o el nombre que queramos y podemos ejecutarlo de la siguiente manera
+                            </p>
+
+                            <p>
+                                go run hola.go # o usar el nombre que pusimos al archivo
+                            </p>
+
+                            <p>
+                                Si todo sale bien deberiamos ver el mensaje hola mundo en la consola
+                            </p>
+
+                            <h2>Bonus configurar camperta para librerias</h2>
+
+                            <p>
+                                Por defecto go instalara librerias externas que configuremos en el escritorio 
+                                del usuario que seria una ruta como /home/usuario pero podemos cambiar esto de la siguiente manera
+                            </p>
+
+                            <p>
+                                sudo mkdir -p /opt/goprojects # o puede ser mejor golibrerias
+                                sudo chmod -R $USER:$USER /opt/goprojects
+                                export GOPATH=/opt/goprojects
+                                export PATH=$PATH:$GOPATH/bin
+                            </p>
+
+                            <p>
+                                De esta manera las instalacion de librerias o paquetes para go se instalar en /opt/goprojecs en 
+                                lugar del escritorio predeterminado del usuario, si lo prefieres puedes ignorar esta configuracion 
+                                puedes seguir guardado todo en el escritorio de usuario
+                            </p>
+
                         </DivContent>
                     </DivArticle>
                 </DivCol>
