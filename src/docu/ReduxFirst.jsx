70bde10c382e12ab0ef1974b3e74fc166ed0ec04
Marco <za19012397@zapopan.tecmm.edu.mx>
2025-06-30 17:25:36 -0600

    feat: Se removieron importaciones que no se usan

--- src/docu/ReduxFirst.jsx
+++ src/docu/ReduxFirst.jsx
@@ -1,6 +1,5 @@
-import { CodeSpace } from "../componentes/codeSpace/CodeSpace";
 import { CodeSpaceV3 } from "../componentes/codeSpace/CodeSpaceV3";
-import { DivArticle, DivCol, DivContent, DivRow, DivSection } from "../componentes/contenedores/contenedores";
+import { DivArticle, DivCol, DivContent, DivRow } from "../componentes/contenedores/contenedores";
 import { Portada } from "../componentes/portada/portada";
 import { TreeMap } from "../componentes/treeMap/TreeMap";
 
