7aa39e2f22a9eb259229f4b10ec5636ae8940a48
Tony <89054896+SteveBartmoss@users.noreply.github.com>
2025-06-29 20:57:10 -0700

    Update workAllDay.jsx
    feat: se agrego contenido para el post

--- src/post/workAllDay.jsx
+++ src/post/workAllDay.jsx
@@ -1,4 +1,9 @@
 export function workAllDay(){
   return(
+    <h1>Si trabajas demasiado, quizás no es bueno</h1>
+
+    <p>Siempre se nos dice que debemos dar el maximo 
+    en el trabajo, pero esto fuera de ser algo bueno puede que simplemente sea 
+      aldo de lo que te arrepientas luego</p>
     )
 }
