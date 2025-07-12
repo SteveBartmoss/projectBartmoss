2f004a311f25c5d439bc6795ee5f9d5ec00b3959
Marco <za19012397@zapopan.tecmm.edu.mx>
2025-07-12 17:49:59 -0600

    feat: Se agregaron backtips para una futura implementacion

--- src/componentes/codeSpace/CodeSpaceV3.jsx
+++ src/componentes/codeSpace/CodeSpaceV3.jsx
@@ -1,21 +1,22 @@
 import { generateUniqueIdV3 } from "../helpers/uid";
-import { decorateToken } from "./decorator/picaso";
+import { decorateToken, getIdent } from "./decorator/picaso";
 import { processCodeV2, sliceCodeV2 } from "./lexer/AdaLove";
 
 
-export function CodeSpaceV3({title,rawCode=""}){
-    return(
+export function CodeSpaceV3({ title, rawCode = "" }) {
+
+    return (
         <div className="code-container">
             <div className="code-space-title">
                 <h1>{title}</h1>
             </div>
             <div className="code-space">
                 {
-                    sliceCodeV2(rawCode).map(lineCode => 
+                    sliceCodeV2(rawCode).map(lineCode =>
                         <p key={generateUniqueIdV3()}>
                             {
-                                processCodeV2(lineCode).map(token => 
-                                    <span key={generateUniqueIdV3()} className={decorateToken(token)}>{token.character}</span>
+                                processCodeV2(lineCode).map(token =>
+                                    <span key={generateUniqueIdV3()} className={`${decorateToken(token)}`} >{token.character}</span>
                                 )
                             }
                         </p>
