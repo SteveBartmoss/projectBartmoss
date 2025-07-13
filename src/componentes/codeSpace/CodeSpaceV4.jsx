ccabf9e6656c1c7bfdb66eb17be3f9cbc557bbb4
Marco <za19012397@zapopan.tecmm.edu.mx>
2025-07-12 21:03:27 -0600

    feat: Se implemento un nuevo lexer para manejar el codigo

---
+++ src/componentes/codeSpace/CodeSpaceV4.jsx
@@ -0,0 +1,34 @@
+import { generateUniqueIdV3 } from "../helpers/uid"
+import { decorateToken } from "./decorator/picaso"
+import { Lexer } from "./lexer/lexer"
+
+
+export function CodeSpaceV4({title, rawCode = ""}){
+
+    const adaLove = new Lexer()
+
+    adaLove.scanner(rawCode)
+
+    console.log(adaLove.listCode)
+
+    return(
+        <div className="code-container">
+            <div className="code-space-title">
+                <h1>{title}</h1>
+            </div>
+            <div className="code-space">
+                {
+                    adaLove.listCode.map(lineCode =>
+                        <p key={generateUniqueIdV3()} style={{paddingLeft: lineCode.ident * 3 + 'rem'}} >
+                            {
+                                lineCode.tokens.map(token =>
+                                    <span key={generateUniqueIdV3()} className={`${decorateToken(token)}`}>{token.character}</span>
+                                )
+                            }
+                        </p>
+                    )
+                }
+            </div>
+        </div>
+    )
+}
