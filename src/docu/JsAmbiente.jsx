89b13cab8764ec8e8cfdca1a4a2e73c2c4396230
Tony <89054896+SteveBartmoss@users.noreply.github.com>
2025-10-28 13:21:01 -0600

    feat: Se implemento el layout de los post

--- src/docu/JsAmbiente.jsx
+++ src/docu/JsAmbiente.jsx
@@ -1,37 +1,35 @@
 import { Portada } from "../componentes/portada/portada"
 import { DivArticle, DivCol, DivContent, DivRow } from "../componentes/contenedores/contenedores"
+import { LayoutPost } from "../layouts/layoutPost"
 
 export function JsAmbiente(){
     return(
         <>
             <Portada imgPortada={"/Assets/steveA.png"} title={"Ambiente de desarrollo para js"} text={"Por Steve 04/04/2025"} />
 
-            <DivRow>
+            <LayoutPost titlePortada={"Ambiente de desarrollo para js"} textPortada={"Por Steve 04/04/2025"} titleMap={'Mapa del sitio'} elementsMap={pageMap}>
 
-                <DivCol>
-                    <DivArticle>
-                        <DivContent>
-                            <h1>Preparar ambiente de desarollo para Js</h1>
+                <h1>Preparar ambiente de desarollo para Js</h1>
 
-                            <p>
-                                Cuando se empieza a desarrollar aplicaciones web con javascript 
-                                es importante preparar la pila de tecnologias para trabajar con 
-                                este lenguaje, en este post veremos como preparar la pila de tecnoliga 
-                                para desarrollar en java script
-                            </p>
+                <p>
+                    Cuando se empieza a desarrollar aplicaciones web con javascript 
+                    es importante preparar la pila de tecnologias para trabajar con 
+                    este lenguaje, en este post veremos como preparar la pila de tecnoliga 
+                    para desarrollar en java script
+                </p>
 
-                            <h2>Entorno de ejecucion</h2>
+                <h2>Entorno de ejecucion</h2>
                             
-                            <p>
-                                Para el entorno de ejecucion tenemos la opcion de nodejs, que si bien 
-                                esta pensado para aplciaciones del lado del servidor tambien funciona 
-                                para levantar proyectos con frameworks de javascript como vue, angular o next.
-                                Tambien es util para ejecutar codigo de javascript ya que si por ejemplo solo necesitas 
-                                correr un codigo de ejemplo o estas resolviendo algun problema y solo quieres 
-                                dar forma al codigo de una forma rapida
-                            </p>
+                <p>
+                    Para el entorno de ejecucion tenemos la opcion de nodejs, que si bien 
+                    esta pensado para aplciaciones del lado del servidor tambien funciona 
+                    para levantar proyectos con frameworks de javascript como vue, angular o next.
+                    Tambien es util para ejecutar codigo de javascript ya que si por ejemplo solo necesitas 
+                    correr un codigo de ejemplo o estas resolviendo algun problema y solo quieres 
+                    dar forma al codigo de una forma rapida
+                </p>
 
-                            <h3>NodeJs</h3>
+                <h3>NodeJs</h3>
 
                             <p>
                                 El entorno de ejecucion que suelo usar para javascript es nodejs, ya que estoy familiarizado con el 
@@ -80,10 +78,8 @@ export function JsAmbiente(){
                                 sudo apt install npm
                             </p>
 
-                        </DivContent>
-                    </DivArticle>
-                </DivCol>
-            </DivRow>
+            </LayoutPost>
+            
         </>
     )
 }
